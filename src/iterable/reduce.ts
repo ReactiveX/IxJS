73564b0bfa83c794ab5ee17e2494b58658277aca
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-02-27 15:47:33 -0500

    fix(TS) - fixing typescript errors

--- src/iterable/reduce.ts
+++ src/iterable/reduce.ts
@@ -1,13 +1,13 @@
 'use strict';
 
-import { IIterable } from '../iterable.interfaces';
+import { IIterable, IIteratorResult } from '../iterable.interfaces';
 
 export function reduce<TSource, TAccumulate>(
     source: IIterable<TSource>, 
-    fn: (acc: TAccumulate, x: TSource, index: number) => TAccumulate, 
-    seed?: TAccumulate): TAccumulate {
-  const hasSeed = arguments.length === 3;
-  let i = 0, hasValue = false, acc, it = source[Symbol.iterator](), next;
+    fn: (acc: TAccumulate | TSource, x: TSource, index: number) => TAccumulate, 
+    seed?: TAccumulate): TAccumulate | TSource {
+  const hasSeed = arguments.length === 3, it = source[Symbol.iterator]();
+  let i = 0, hasValue = false, acc: TAccumulate | TSource, next: IIteratorResult<TSource>;
   hasSeed && i++;
   while (!(next = it.next()).done) {
     if (hasValue || (hasValue = hasSeed)) {
