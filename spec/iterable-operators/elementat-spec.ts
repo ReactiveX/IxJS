6beedb7c5609ae1e59bf4b88c370e5e502b04cce
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-05-23 16:49:29 -0400

    feat(operators) - adding elementAt

---
+++ spec/iterable-operators/elementat-spec.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+import * as test from 'tape';
+import { elementAt } from '../../dist/cjs/iterable/elementat';
+
+test('Iterable#elementAt empty returns undefined', t => {
+  const res = elementAt<number>([], 0);
+  t.equal(res, undefined);
+  t.end();
+});
+
+test('Iterable#elementAt single value first index', t => {
+  const res = elementAt([42], 0);
+  t.equal(res, 42);
+  t.end();
+});
+
+test('Iterable#elementAt single value invalid index', t => {
+  const res = elementAt([42], 1);
+  t.equal(res, undefined);
+  t.end();
+});
+
+test('Iterable#elementAt multiple values valid index', t => {
+  const res = elementAt([1, 42, 3], 1);
+  t.equal(res, 42);
+  t.end();
+});
+
+test('Iterable#elementAt multiple values invalid index', t => {
+  const res = elementAt([1, 42, 3], 7);
+  t.equal(res, undefined);
+  t.end();
+});
