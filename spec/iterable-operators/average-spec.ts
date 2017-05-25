916845f0c7325a5a2db208dabda7d48312d123b4
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-05-24 21:53:01 -0400

    feat(operators) - add average

---
+++ spec/iterable-operators/average-spec.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+import * as test from 'tape';
+import { average } from '../../dist/cjs/iterable/average';
+
+test('Iterable#average empty', t => {
+  t.throws(() => average([]));
+  t.end();
+});
+
+test('Iterable#average', t => {
+  const res = average([1, 2, 3]);
+  t.equal(res, 2);
+  t.end();
+});
+
+test('Iterable#average with selector empty', t => {
+  t.throws(() => average<number>([], x => x * 2));
+  t.end();
+});
+
+test('Iterable#average with selector', t => {
+  const res = average([1, 2, 3], x => x * 2);
+  t.equal(res, 4);
+  t.end();
+});
