cca3b1361b271436307648dcfb95bb16fb43b030
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-05-25 11:35:05 -0400

    feat(operators) - add sum

---
+++ spec/iterable-operators/sum-spec.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+import * as test from 'tape';
+import { sum } from '../../dist/cjs/iterable/sum';
+
+test('Iterable#sum laws', t => {
+  const xs = [1, 2, 3];
+  t.equal(sum(xs), sum(xs, x => x));
+  t.end();
+});
+
+test('Iterable#sum no selector empty', t => {
+  const xs: number[] = [];
+  t.equal(sum(xs), 0);
+  t.end();
+});
+
+test('#Iterable#sum no selector', t => {
+  const xs: number[] = [1, 2, 3];
+  t.equal(sum(xs), 6);
+  t.end();
+});
+
+test('Iterable#sum with selector empty', t => {
+  const xs: number[] = [];
+  t.equal(sum(xs, x => x * 2), 0);
+  t.end();
+});
+
+test('#Iterable#sum with selector', t => {
+  const xs: number[] = [1, 2, 3];
+  t.equal(sum(xs, x => x * 2), 12);
+  t.end();
+});
