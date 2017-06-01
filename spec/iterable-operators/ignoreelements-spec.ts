9b6bd6c52c6654131a0d5b1b2f2c18b050ddfd99
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-01 17:19:31 -0400

    feat(ignoreElements) - fix linting for test

--- spec/iterable-operators/ignoreelements-spec.ts
+++ spec/iterable-operators/ignoreelements-spec.ts
@@ -10,8 +10,8 @@ import { tap } from '../../dist/cjs/iterable/tap';
 test('Iterable#ignoreElements has side effects', t => {
   let n = 0;
   forEach(
-    take(ignoreElements(tap(range(0, 10), { next: () => n++})), 5),
-    () => { }
+    take(ignoreElements(tap(range(0, 10), { next: () => n++})), 5), 
+    () => { /* tslint:disable-next-line:no-empty */ }
   );
 
   t.equal(10, n);
