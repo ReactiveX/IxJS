7b99d8a7ef22037b98f53621533cabc8dfc282f5
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-01 18:38:29 -0400

    feat(intersect) - finish unit tests for intersect

--- spec/iterable-operators/intersect-spec.ts
+++ spec/iterable-operators/intersect-spec.ts
@@ -15,3 +15,16 @@ test('Iterable#union with default comparer', t => {
   noNext(t, it);
   t.end();
 });
+
+test('Iterable#union with custom comparer', t => {
+  const comparer = (x: number, y: number) => Math.abs(x) === Math.abs(y);
+  const xs = [1, 2, -3];
+  const ys = [3, 5, -1, 4];
+  const res = intersect(xs, ys, comparer);
+
+  const it = res[Symbol.iterator]();
+  hasNext(t, it, 1);
+  hasNext(t, it, -3);
+  noNext(t, it);
+  t.end();
+});
