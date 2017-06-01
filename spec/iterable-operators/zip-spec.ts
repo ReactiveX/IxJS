1168ab298891b87b830d50f92227f3afafd93675
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-01 17:19:13 -0400

    feat(zip) - add zip opterator

---
+++ spec/iterable-operators/zip-spec.ts
@@ -0,0 +1,75 @@
+'use strict';
+
+import * as test from 'tape';
+import { zip } from '../../dist/cjs/iterable/zip';
+import { _throw } from '../../dist/cjs/iterable/throw';
+import { hasNext, noNext } from '../iterablehelpers';
+
+test('Iterable#zip equal length', t => {
+  const xs = [1, 2, 3];
+  const ys = [4, 5, 6];
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.iterator]();
+  hasNext(t, it, 1 * 4);
+  hasNext(t, it, 2 * 5);
+  hasNext(t, it, 3 * 6);
+  noNext(t, it);
+  t.end();
+});
+
+test('Iterable#zip left longer', t => {
+  const xs = [1, 2, 3, 4];
+  const ys = [4, 5, 6];
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.iterator]();
+  hasNext(t, it, 1 * 4);
+  hasNext(t, it, 2 * 5);
+  hasNext(t, it, 3 * 6);
+  noNext(t, it);
+  t.end();
+});
+
+test('Iterable#zip right longer', t => {
+  const xs = [1, 2, 3];
+  const ys = [4, 5, 6, 7];
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.iterator]();
+  hasNext(t, it, 1 * 4);
+  hasNext(t, it, 2 * 5);
+  hasNext(t, it, 3 * 6);
+  noNext(t, it);
+  t.end();
+});
+
+test('Iterable#zip left throws', t => {
+  const xs = _throw<number>(new Error());
+  const ys = [4, 5, 6];
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.iterator]();
+  t.throws(() => it.next());
+  t.end();
+});
+
+test('Iterable#zip right throws', t => {
+  const xs = [1, 2, 3];
+  const ys = _throw<number>(new Error());
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.iterator]();
+  t.throws(() => it.next());
+  t.end();
+});
+
+test('Iterable#zip selector throws', t => {
+  const xs = [1, 2, 3];
+  const ys = [4, 5, 6];
+  const res = zip(xs, ys, (x, y) => { if (x > 0) { throw new Error(); } return x * y; });
+
+  const it = res[Symbol.iterator]();
+  t.throws(() => it.next());
+  t.end();
+});
