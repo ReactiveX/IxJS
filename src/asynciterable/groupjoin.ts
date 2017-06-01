283b87129106820792875ac3c9867624b16c3bd3
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-01 14:33:02 -0400

    feat(groupJoin) - add async groupJoin

---
+++ src/asynciterable/groupjoin.ts
@@ -0,0 +1,51 @@
+'use strict';
+
+import { AsyncIterableX } from '../asynciterable';
+import { createGrouping } from './_grouping';
+import { empty } from '../iterable/empty';
+import { identity } from '../internal/identity';
+
+class GroupJoinAsyncIterable<TOuter, TInner, TKey, TResult> extends AsyncIterableX<TResult> {
+  private _outer: AsyncIterable<TOuter>;
+  private _inner: AsyncIterable<TInner>;
+  private _outerSelector: (value: TOuter) => TKey;
+  private _innerSelector: (value: TInner) => TKey;
+  private _resultSelector: (outer: TOuter, inner: Iterable<TInner>) => TResult;
+
+  constructor(
+      outer: AsyncIterable<TOuter>,
+      inner: AsyncIterable<TInner>,
+      outerSelector: (value: TOuter) => TKey,
+      innerSelector: (value: TInner) => TKey,
+      resultSelector: (outer: TOuter, inner: Iterable<TInner>) => TResult) {
+    super();
+    this._outer = outer;
+    this._inner = inner;
+    this._outerSelector = outerSelector;
+    this._innerSelector = innerSelector;
+    this._resultSelector = resultSelector;
+  }
+
+  async *[Symbol.asyncIterator]() {
+    const map = await createGrouping(this._inner, this._innerSelector, identity);
+    for await (let outerElement of this._outer) {
+      const outerKey = this._outerSelector(outerElement);
+      const innerElements = map.has(outerKey) ? <Iterable<TInner>>map.get(outerKey) : empty<TInner>();
+      yield this._resultSelector(outerElement, innerElements);
+    }
+  }
+}
+
+export function groupJoin<TOuter, TInner, TKey, TResult>(
+    outer: AsyncIterable<TOuter>,
+    inner: AsyncIterable<TInner>,
+    outerSelector: (value: TOuter) => TKey,
+    innerSelector: (value: TInner) => TKey,
+    resultSelector: (outer: TOuter, inner: Iterable<TInner>) => TResult): AsyncIterableX<TResult> {
+  return new GroupJoinAsyncIterable<TOuter, TInner, TKey, TResult>(
+    outer,
+    inner,
+    outerSelector,
+    innerSelector,
+    resultSelector);
+}
