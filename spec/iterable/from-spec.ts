2b625c4bedbb1fe5ec3e20785f1ba5ff1eb0c325
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-01 21:29:31 -0400

    feat(from) - adds the from operator

---
+++ spec/iterable/from-spec.ts
@@ -0,0 +1,62 @@
+'use strict';
+
+import * as test from 'tape';
+import { from } from '../../dist/cjs/iterable/from';
+import { hasNext, noNext } from '../iterablehelpers';
+
+test('Iterable#from from array/iterable', t => {
+  const xs = [1, 2, 3];
+  const res = from(xs);
+
+  const it = res[Symbol.iterator]();
+  hasNext(t, it, 1);
+  hasNext(t, it, 2);
+  hasNext(t, it, 3);
+  noNext(t, it);
+  t.end();
+});
+
+test('Iterable#from from array/iterable with selector', t => {
+  const xs = [1, 2, 3];
+  const res = from(xs, (x, i) => x + i);
+
+  const it = res[Symbol.iterator]();
+  hasNext(t, it, 1);
+  hasNext(t, it, 3);
+  hasNext(t, it, 5);
+  noNext(t, it);
+  t.end();
+});
+
+test('Iterable#from from empty array/iterable', t => {
+  const xs: number[] = [];
+  const res = from(xs);
+
+  const it = res[Symbol.iterator]();
+  noNext(t, it);
+  t.end();
+});
+
+test('Iterable#from from array-like', t => {
+  const xs = { length: 3 };
+  const res = from(xs);
+
+  const it = res[Symbol.iterator]();
+  hasNext(t, it, undefined);
+  hasNext(t, it, undefined);
+  hasNext(t, it, undefined);
+  noNext(t, it);
+  t.end();
+});
+
+test('Iterable#from from array-like with selector', t => {
+  const xs = { length: 3 };
+  const res = from(xs, (x, i) => i);
+
+  const it = res[Symbol.iterator]();
+  hasNext(t, it, 0);
+  hasNext(t, it, 1);
+  hasNext(t, it, 2);
+  noNext(t, it);
+  t.end();
+});
