b012c26f3b69396c3e7966ec55cedb7c4d107617
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-05 23:44:15 -0400

    feat(single) - adds single spec

---
+++ spec/asynciterable-operators/single-spec.ts
@@ -0,0 +1,61 @@
+'use strict';
+
+import * as test from 'tape';
+import { empty } from '../../dist/cjs/asynciterable/empty';
+import { of } from '../../dist/cjs/asynciterable/of';
+import { single } from '../../dist/cjs/asynciterable/single';
+
+test('AsyncIterable#single no predicate empty returns undefined', async (t: test.Test) => {
+  const xs = empty<number>();
+  const res = await single(xs);
+  t.equal(res, undefined);
+  t.end();
+});
+
+test('AsyncIterable#single with predicate empty returns undefined', async (t: test.Test) => {
+  const xs = empty<number>();
+  const res = await single(xs, async () => true);
+  t.equal(res, undefined);
+  t.end();
+});
+
+test('AsyncIterable#single predicate miss', async (t: test.Test) => {
+  const xs = of(42);
+  const res = await single(xs, x => x % 2 !== 0);
+  t.equal(res, undefined);
+  t.end();
+});
+
+test('AsyncIterable#single no predicate hit', async (t: test.Test) => {
+  const xs = of(42);
+  const res = await single(xs);
+  t.equal(res, 42);
+  t.end();
+});
+
+test('AsyncIterable#single predicate hit', async (t: test.Test) => {
+  const xs = of(42);
+  const res = await single(xs, x => x % 2 === 0);
+  t.equal(res, 42);
+  t.end();
+});
+
+test('AsyncIterable#single no predicate multiple throws error', async (t: test.Test) => {
+  const xs = of(42, 45, 90);
+  try {
+    await single(xs);
+  } catch (e) {
+    t.assert(e != null);
+  }
+  t.end();
+});
+
+test('AsyncIterable#single with predicate multiple throws error', async (t: test.Test) => {
+  const xs = of(42, 45, 90);
+  try {
+    await single(xs, async x => x % 2 === 0);
+  } catch (e) {
+    t.assert(e != null);
+  }
+  t.end();
+});
