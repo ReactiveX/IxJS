a91a1b78b73c5934e50bba4a9ee86c42f8c9d533
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-06 19:57:14 -0400

    feat(ignoreElements) - add more async

--- spec/asynciterable-operators/ignoreelements.ts
+++ spec/asynciterable-operators/ignoreelements.ts
@@ -10,8 +10,8 @@ import { tap } from '../../dist/cjs/asynciterable/tap';
 test('Iterable#ignoreElements has side effects', async t => {
   let n = 0;
   await forEach(
-    take(ignoreElements(tap(range(0, 10), { next: () => { n++; } })), 5),
-    () => { /* tslint:disable-next-line:no-empty */ }
+    take(ignoreElements(tap(range(0, 10), { next: async () => { n++; } })), 5),
+    async () => { /* tslint:disable-next-line:no-empty */ }
   );
 
   t.equal(10, n);
