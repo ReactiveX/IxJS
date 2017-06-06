4cd40d75e00af92991e18306e3eedc2a749f14ca
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-06 15:13:53 -0400

    feat(flatMap) - add async flatMap tests

---
+++ spec/asynciterable-operators/flatmap-spec.ts
@@ -0,0 +1,74 @@
+'use strict';
+
+import * as test from 'tape';
+import { flatMap } from '../../dist/cjs/asynciterable/flatmap';
+import { of } from '../../dist/cjs/asynciterable/of';
+import { range } from '../../dist/cjs/asynciterable/range';
+import { _throw } from '../../dist/cjs/asynciterable/throw';
+import { hasNext, noNext } from '../asynciterablehelpers';
+
+test('Iterable#flatMap with range', async t => {
+  const xs = of(1, 2, 3);
+  const ys = flatMap(xs, async x => range(0, x));
+
+  const it = ys[Symbol.asyncIterator]();
+  hasNext(t, it, 0);
+  hasNext(t, it, 0);
+  hasNext(t, it, 1);
+  hasNext(t, it, 0);
+  hasNext(t, it, 1);
+  hasNext(t, it, 2);
+  noNext(t, it);
+  t.end();
+});
+
+test('Iterable#flatMap selector returns throw', async t => {
+  const err = new Error();
+  const xs = of(1, 2, 3);
+  const ys = flatMap(xs, async x => x < 3 ? range(0, x) : _throw(err));
+
+  const it = ys[Symbol.asyncIterator]();
+  hasNext(t, it, 0);
+  hasNext(t, it, 0);
+  hasNext(t, it, 1);
+  try {
+    await it.next();
+  } catch (e) {
+    t.assert(e != null);
+  }
+  t.end();
+});
+
+test('Iterable#flatMap with error throws', async t => {
+  const err = new Error();
+  const xs = _throw<number>(err);
+  const ys = flatMap(xs, x => range(0, x));
+
+  const it = ys[Symbol.asyncIterator]();
+  try {
+    await it.next();
+  } catch (e) {
+    t.assert(e != null);
+  }
+  t.end();
+});
+
+test('Iterable#flatMap selector throws error', async t => {
+  const err = new Error();
+  const xs = of(1, 2, 3);
+  const ys = flatMap(xs, async x => {
+    if (x < 3) { return range(0, x); }
+    throw err;
+  });
+
+  const it = ys[Symbol.asyncIterator]();
+  hasNext(t, it, 0);
+  hasNext(t, it, 0);
+  hasNext(t, it, 1);
+  try {
+    await it.next();
+  } catch (e) {
+    t.assert(e != null);
+  }
+  t.end();
+});
