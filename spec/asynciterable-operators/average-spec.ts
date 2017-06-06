223c197b733d03f0e58e98b69508c149f3e1717b
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-06 09:29:39 -0400

    feat(average) - adds average async operator

---
+++ spec/asynciterable-operators/average-spec.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+import * as test from 'tape';
+import { average } from '../../dist/cjs/asynciterable/average';
+import { empty } from '../../dist/cjs/asynciterable/empty';
+import { of } from '../../dist/cjs/asynciterable/of';
+
+test('Iterable#average empty', async (t: test.Test) => {
+  const xs = empty<number>();
+  try {
+    await average(xs);
+  } catch (e) {
+    t.assert(e != null);
+  }
+  t.end();
+});
+
+test('Iterable#average', async (t: test.Test) => {
+  const res = await average(of(1, 2, 3));
+  t.equal(res, 2);
+  t.end();
+});
+
+test('Iterable#average with selector empty', async (t: test.Test) => {
+  const xs = empty<number>();
+  try {
+    await average(xs, async x => x * 2);
+  } catch (e) {
+    t.assert(e != null);
+  }
+  t.end();
+});
+
+test('Iterable#average with selector', async (t: test.Test) => {
+  const res = await average(of(1, 2, 3), x => x * 2);
+  t.equal(res, 4);
+  t.end();
+});
+
+test('Iterable#average laws', async (t: test.Test) => {
+  const xs = of(1, 2, 3);
+  t.equal(await average(xs), await average(xs, x => x));
+  t.end();
+});
