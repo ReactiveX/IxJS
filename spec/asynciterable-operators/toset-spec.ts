b8eed2242af19e3917f15ee66d7a76e88b0f220e
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-05 23:59:16 -0400

    feat(toSet) - adds async toSet

---
+++ spec/asynciterable-operators/toset-spec.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+import * as test from 'tape';
+import { empty } from '../../dist/cjs/asynciterable/empty';
+import { from } from '../../dist/cjs/asynciterable/from';
+import { sequenceEqual } from '../../dist/cjs/iterable/sequenceequal';
+import { toSet } from '../../dist/cjs/asynciterable/toset';
+
+test('Iterable#toSet non-empty', async (t: test.Test) => {
+  const xs = [1, 2, 3, 4, 5];
+  const ys = from(xs);
+  const res = await toSet(ys);
+  t.true(sequenceEqual(res, xs));
+  t.end();
+});
+
+test('Iterable#toSet empty', async (t: test.Test) => {
+  const xs = empty<number>();
+  const res = await toSet(xs);
+  t.equal(res.size, 0);
+  t.end();
+});
+
+test('Iterable#toSet trims', async (t: test.Test) => {
+  const xs = from([1, 2, 3, 3, 2, 1]);
+  const ys = [1, 2, 3];
+  const res = await toSet(xs);
+  t.true(sequenceEqual(res, ys));
+  t.end();
+});
