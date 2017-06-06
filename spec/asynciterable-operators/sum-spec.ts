816ff8ea8d9c34d0a2608e4a1f5160a87be512eb
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-06 12:46:01 -0400

    feat(sum) - adding more tests for async sum

---
+++ spec/asynciterable-operators/sum-spec.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+import * as test from 'tape';
+import { empty } from '../../dist/cjs/asynciterable/empty';
+import { of } from '../../dist/cjs/asynciterable/of';
+import { sum } from '../../dist/cjs/asynciterable/sum';
+
+test('Iterable#sum laws', async t => {
+  const xs = of(1, 2, 3);
+  t.equal(await sum(xs), await sum(xs, async x => x));
+  t.end();
+});
+
+test('Iterable#sum no selector empty', async t => {
+  const xs = empty<number>();
+  const res = await sum(xs);
+  t.equal(res, 0);
+  t.end();
+});
+
+test('#Iterable#sum no selector', async t => {
+  const xs = of(1, 2, 3);
+  const res = await sum(xs);
+  t.equal(res, 6);
+  t.end();
+});
+
+test('Iterable#sum with selector empty', async t => {
+  const xs = empty<number>();
+  const res = await sum(xs, async x => x * 2);
+  t.equal(res, 0);
+  t.end();
+});
+
+test('#Iterable#sum with selector', async t => {
+  const xs = of(1, 2, 3);
+  const res = await sum(xs, async x => x * 2);
+  t.equal(res, 12);
+  t.end();
+});
