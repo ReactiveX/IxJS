5722f72f6fe2a8e81d0387e152eb79b152d066a8
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-07 23:10:16 -0400

    feat(map) - add async map

---
+++ spec/asynciterable-operators/map-spec.ts
@@ -0,0 +1,70 @@
+'use strict';
+
+import  * as test  from 'tape';
+import { empty } from '../../dist/cjs/asynciterable/empty';
+import { map } from '../../dist/cjs/asynciterable/map';
+import { of } from '../../dist/cjs/asynciterable/of';
+import { sequenceEqual } from '../../dist/cjs/asynciterable/sequenceequal';
+
+test('AsyncIterable#map single element', async t => {
+  const source = of({ name: 'Frank', custId: 98088 });
+  const expected = of('Frank');
+
+  t.true(await sequenceEqual(expected, map(source, x => x.name)));
+  t.end();
+});
+
+test('AsyncIterable#map maps property', t => {
+  const source = of<any>(
+    { name: 'Frank', custId: 98088 },
+    { name: 'Bob', custId: 29099 },
+    { name: 'Chris', custId: 39033 },
+    { name: null, custId: 30349 },
+    { name: 'Frank', custId: 39030 },
+  );
+  const expected = of('Frank', 'Bob', 'Chris', null, 'Frank');
+
+  t.true(sequenceEqual(expected, map(source, x => x.name)));
+  t.end();
+});
+
+test('AsyncIterable#map empty', t => {
+  t.true(sequenceEqual(empty<number>(), map(empty<string>(), (s, i) => s.length + i)));
+  t.end();
+});
+
+test('AsyncIterable#map map property using index', async t => {
+  const source = of(
+    { name: 'Frank', custId: 98088 },
+    { name: 'Bob', custId: 29099 },
+    { name: 'Chris', custId: 39033 }
+  );
+  const expected = of('Frank', null, null);
+
+  t.true(await sequenceEqual(expected, map(source, (x, i) => i === 0 ? x.name : null)));
+  t.end();
+});
+
+test('AsyncIterable#map map property using index on last', async t => {
+  const source = of(
+    { name: 'Frank', custId: 98088 },
+    { name: 'Bob', custId: 29099 },
+    { name: 'Chris', custId: 39033 },
+    { name: 'Bill', custId: 30349 },
+    { name: 'Frank', custId: 39030 },
+  );
+  const expected = of(null, null, null, null, 'Frank');
+
+  t.true(await sequenceEqual(expected, map(source, (x, i) => i === 4 ? x.name : null)));
+  t.end();
+});
+
+test('AsyncIterable#map execution is deferred', async t => {
+  let fnCalled = false;
+  const source = of(() => { fnCalled = true; return 1; });
+
+  map(source, x => x());
+
+  t.false(fnCalled);
+  t.end();
+});
