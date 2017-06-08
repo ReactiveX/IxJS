8a3090f6b15ebc4ad857d1c6acb032e372bf03a8
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-08 01:55:41 -0400

    feat(union) - add async union

---
+++ spec/asynciterable-operators/union-spec.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+import * as test from 'tape';
+import { of } from '../../dist/cjs/asynciterable/of';
+import { union } from '../../dist/cjs/asynciterable/union';
+import { hasNext, noNext } from '../asynciterablehelpers';
+
+test('AsyncIterable#union with default comparer', async t => {
+  const xs = of(1, 2, 3);
+  const ys = of(3, 5, 1, 4);
+  const res = union(xs, ys);
+
+  const it = res[Symbol.asyncIterator]();
+  await hasNext(t, it, 1);
+  await hasNext(t, it, 2);
+  await hasNext(t, it, 3);
+  await hasNext(t, it, 5);
+  await hasNext(t, it, 4);
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncIterable#union with custom comparer', async t => {
+  const comparer = (x: number, y: number) => Math.abs(x) === Math.abs(y);
+  const xs = of(1, 2, -3);
+  const ys = of(3, 5, -1, 4);
+  const res = union(xs, ys, comparer);
+
+  const it = res[Symbol.asyncIterator]();
+  await hasNext(t, it, 1);
+  await hasNext(t, it, 2);
+  await hasNext(t, it, -3);
+  await hasNext(t, it, 5);
+  await hasNext(t, it, 4);
+  await noNext(t, it);
+  t.end();
+});
