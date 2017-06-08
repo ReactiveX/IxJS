986a98e36b078bfbb06663186e57fe09d68e08ef
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-08 15:14:21 -0400

    feat(memoize) - adds async memoize

---
+++ spec/asynciterable-operators/memoize-spec.ts
@@ -0,0 +1,215 @@
+'use strict';
+
+import  * as test  from 'tape';
+import { concat } from '../../dist/cjs/asynciterable/concat';
+import { every } from '../../dist/cjs/asynciterable/every';
+import { from } from '../../dist/cjs/asynciterable/from';
+import { map } from '../../dist/cjs/asynciterable/map';
+import { memoize } from '../../dist/cjs/asynciterable/memoize';
+import { range } from '../../dist/cjs/asynciterable/range';
+import { sequenceEqual } from '../../dist/cjs/asynciterable/sequenceequal';
+import { take } from '../../dist/cjs/asynciterable/take';
+import { tap } from '../../dist/cjs/asynciterable/tap';
+import { _throw } from '../../dist/cjs/asynciterable/throw';
+import { toArray } from '../../dist/cjs/asynciterable/toarray';
+import { zip } from '../../dist/cjs/asynciterable/zip';
+import { hasNext  , noNext } from '../asynciterablehelpers';
+
+async function* tick(t: (x: number) => void | Promise<void>) {
+  let i = 0;
+  while (1) {
+    await t(i);
+    yield i++;
+  }
+}
+
+test('AsyncIterable#memoize memoizes effects', async t => {
+  let n = 0;
+  const rng = memoize(tick(async i => { n += i; }));
+
+  const it1 = rng[Symbol.asyncIterator]();
+  const it2 = rng[Symbol.asyncIterator]();
+
+  await hasNext(t, it1, 0);
+  t.equal(0, n);
+
+  await hasNext(t, it1, 1);
+  t.equal(1, n);
+
+  await hasNext(t, it1, 2);
+  t.equal(3, n);
+  await hasNext(t, it2, 0);
+  t.equal(3, n);
+
+  await hasNext(t, it1, 3);
+  t.equal(6, n);
+  await hasNext(t, it2, 1);
+  t.equal(6, n);
+
+  await hasNext(t, it2, 2);
+  t.equal(6, n);
+  await hasNext(t, it2, 3);
+  t.equal(6, n);
+
+  await hasNext(t, it2, 4);
+  t.equal(10, n);
+  await hasNext(t, it1, 4);
+  t.equal(10, n);
+
+  t.end();
+});
+
+test('AsyncIterable#memoize single', async t => {
+  const rng = memoize(range(0, 5));
+
+  const it1 = rng[Symbol.asyncIterator]();
+
+  await hasNext(t, it1, 0);
+  await hasNext(t, it1, 1);
+  await hasNext(t, it1, 2);
+  await hasNext(t, it1, 3);
+  await hasNext(t, it1, 4);
+  await noNext(t, it1);
+
+  t.end();
+});
+
+test('AsyncIterable#memoize order of operations', async t => {
+  const rng = memoize(range(0, 5));
+
+  const it1 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it1, 0);
+  await hasNext(t, it1, 1);
+  await hasNext(t, it1, 2);
+  await hasNext(t, it1, 3);
+  await hasNext(t, it1, 4);
+  await noNext(t, it1);
+
+  const it2 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it2, 0);
+  await hasNext(t, it2, 1);
+  await hasNext(t, it2, 2);
+  await hasNext(t, it2, 3);
+  await hasNext(t, it2, 4);
+  await noNext(t, it2);
+
+  t.end();
+});
+
+test('AsyncIterable#memoize second early', async t => {
+  const rng = memoize(range(0, 5));
+
+  const it1 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it1, 0);
+  await hasNext(t, it1, 1);
+  await hasNext(t, it1, 2);
+
+  const it2 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it1, 3);
+  await hasNext(t, it2, 0);
+  await hasNext(t, it2, 1);
+  await hasNext(t, it1, 4);
+  await hasNext(t, it2, 2);
+  await noNext(t, it1);
+
+  await hasNext(t, it2, 3);
+  await hasNext(t, it2, 4);
+  await noNext(t, it2);
+
+  t.end();
+});
+
+test('AsyncIterable#memoize max two readers', async t => {
+  const rng = memoize(range(0, 5), 2);
+
+  const it1 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it1, 0);
+  await hasNext(t, it1, 1);
+  await hasNext(t, it1, 2);
+
+  const it2 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it2, 0);
+  await hasNext(t, it2, 1);
+  await hasNext(t, it2, 2);
+
+  const it3 = rng[Symbol.asyncIterator]();
+  try {
+    await it3.next();
+  } catch (e) {
+    t.assert(e != null);
+  }
+
+  t.end();
+});
+
+test('AsyncIterable#memoize concat with error', async t => {
+  const error = new Error();
+  const rng = memoize(concat(range(0, 2), _throw(error)));
+
+  const it1 = rng[Symbol.asyncIterator]();
+  const it2 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it1, 0);
+  await hasNext(t, it1, 1);
+  try {
+    await it1.next();
+  } catch (e) {
+    t.same(error, e);
+  }
+
+  await hasNext(t, it2, 0);
+  await hasNext(t, it2, 1);
+  try {
+    await it2.next();
+  } catch (e) {
+    t.same(error, e);
+  }
+
+  t.end();
+});
+
+function getRandom() {
+  let min = 0, max = Math.pow(2, 53) - 1;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+async function* rand() {
+  while (1) {
+    yield getRandom();
+  }
+}
+
+test('AsyncIterable#memoize should share effects of random', async t => {
+  const rnd = memoize(take(rand(), 100));
+  t.true(await every(zip(rnd, rnd, async (l, r) => l === r), async x => x));
+  t.end();
+});
+
+test('AsyncIterable#memoize with selector', async t => {
+  let n = 0;
+  const res = await toArray(
+    memoize(
+      tap(range(0, 4), { next: async () => { n++; } }),
+      undefined,
+      xs => take(zip(xs, xs, async (l, r) => l + r), 4)
+    )
+  );
+
+  t.true(await sequenceEqual(from(res), map(range(0, 4), async x => x * 2)));
+  t.equal(4, n);
+  t.end();
+});
+
+test('AsyncIterable#memoize limited with selector', async t => {
+  let n = 0;
+  const res = await toArray(
+    memoize(
+      tap(range(0, 4), { next: async () => { n++; } }),
+      2,
+      xs => take(zip(xs, xs, async (l, r) => l + r), 4)
+    )
+  );
+
+  t.true(await sequenceEqual(from(res), map(range(0, 4), async x => x * 2)));
+  t.equal(4, n);
+  t.end();
+});
---
+++ src/asynciterable/memoize.ts
@@ -0,0 +1,78 @@
+'use strict';
+
+import { AsyncIterableX } from '../asynciterable';
+import { IRefCountList, MaxRefCountList, RefCountList } from '../iterable/_refcountlist';
+import { create } from './create';
+
+class MemoizeAsyncBuffer<T> extends AsyncIterableX<T> {
+  private _source: AsyncIterator<T>;
+  private _buffer: IRefCountList<T>;
+  private _error: any;
+  private _stopped: boolean = false;
+
+  constructor(source: AsyncIterator<T>, buffer: IRefCountList<T>) {
+    super();
+    this._source = source;
+    this._buffer = buffer;
+  }
+
+  async *[Symbol.asyncIterator]() {
+    let i = 0;
+    try {
+      while (1) {
+        let hasValue = false, current = <T>{};
+        if (i >= this._buffer.count) {
+          if (!this._stopped) {
+            try {
+              let next = await this._source.next();
+              hasValue = !next.done;
+              if (hasValue) { current = next.value; }
+            } catch (e) {
+              this._error = e;
+              this._stopped = true;
+            }
+          }
+
+          if (this._stopped) {
+            throw this._error;
+          }
+
+          if (hasValue) { this._buffer.push(current); }
+        } else {
+          hasValue = true;
+        }
+
+        if (hasValue) {
+          yield this._buffer.get(i);
+        } else {
+          break;
+        }
+
+        i++;
+      }
+    } finally {
+      this._buffer.done();
+    }
+  }
+}
+export function memoize<TSource>(
+    source: AsyncIterable<TSource>,
+    readerCount?: number): AsyncIterableX<TSource>;
+export function memoize<TSource, TResult>(
+    source: AsyncIterable<TSource>,
+    readerCount?: number,
+    selector?: (value: AsyncIterable<TSource>) => AsyncIterable<TResult>): AsyncIterableX<TResult>;
+export function memoize<TSource, TResult = TSource>(
+    source: AsyncIterable<TSource>,
+    readerCount: number = -1,
+    selector?: (value: AsyncIterable<TSource>) => AsyncIterable<TResult>): AsyncIterableX<TSource | TResult> {
+  if (readerCount === -1 && !selector) {
+    return new MemoizeAsyncBuffer<TSource>(source[Symbol.asyncIterator](), new MaxRefCountList<TSource>());
+  }
+
+  if (readerCount !== -1 && !selector) {
+    return new MemoizeAsyncBuffer<TSource>(source[Symbol.asyncIterator](), new RefCountList<TSource>(readerCount));
+  }
+
+  return create<TSource | TResult>(() => selector!(memoize(source, readerCount))[Symbol.asyncIterator]());
+}
