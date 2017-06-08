5e63aa0802c8f2a3e7876ca6a00537c64327cab4
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-08 01:15:59 -0400

    feat(share) - add async share

---
+++ spec/asynciterable-operators/share-spec.ts
@@ -0,0 +1,70 @@
+'use strict';
+
+import  * as test  from 'tape';
+import { range } from '../../dist/cjs/asynciterable/range';
+import { sequenceEqual } from '../../dist/cjs/iterable/sequenceequal';
+import { share } from '../../dist/cjs/asynciterable/share';
+import { take } from '../../dist/cjs/asynciterable/take';
+import { tap } from '../../dist/cjs/asynciterable/tap';
+import { toArray } from '../../dist/cjs/asynciterable/toarray';
+import { zip } from '../../dist/cjs/asynciterable/zip';
+import { hasNext, noNext } from '../asynciterablehelpers';
+
+test('AsyncIterable#share single', async t => {
+  const rng = share(range(0, 5));
+
+  const it = rng[Symbol.asyncIterator]();
+  await hasNext(t, it, 0);
+  await hasNext(t, it, 1);
+  await hasNext(t, it, 2);
+  await hasNext(t, it, 3);
+  await hasNext(t, it, 4);
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncIterable#share shared exhausts in the beginning', async t => {
+  const rng = share(range(0, 5));
+
+  const it1 = rng[Symbol.asyncIterator]();
+  const it2 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it1, 0);
+  await hasNext(t, it2, 1);
+  await hasNext(t, it1, 2);
+  await hasNext(t, it2, 3);
+  await hasNext(t, it1, 4);
+  await noNext(t, it1);
+  await noNext(t, it2);
+  t.end();
+});
+
+test('AsyncIterable#share shared exhausts any time', async t => {
+  const rng = share(range(0, 5));
+
+  const it1 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it1, 0);
+  await hasNext(t, it1, 1);
+  await hasNext(t, it1, 2);
+
+  const it2 = rng[Symbol.asyncIterator]();
+  await hasNext(t, it2, 3);
+  await hasNext(t, it2, 4);
+
+  await noNext(t, it1);
+  await noNext(t, it2);
+  t.end();
+});
+
+test('AsyncIterable#share with selector', async t => {
+  let n = 0;
+  const res = await toArray(
+    share(
+      tap(range(0, 10), { next: async () => { n++;} }),
+      xs => take(zip(xs, xs, (l, r) => l + r), 4)
+    )
+  );
+
+  t.true(sequenceEqual(res, [0 + 1, 2 + 3, 4 + 5, 6 + 7]));
+  t.equal(8, n);
+  t.end();
+});
