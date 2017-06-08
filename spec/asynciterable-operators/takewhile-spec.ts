7cdadd85d91e5a5f2347d47f28c0ce423f0f4124
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-08 01:42:51 -0400

    feat(takeWhile) - adds async takeWhile

---
+++ spec/asynciterable-operators/takewhile-spec.ts
@@ -0,0 +1,64 @@
+'use strict';
+
+import * as test from 'tape';
+import { of } from '../../dist/cjs/asynciterable/of';
+import { takeWhile } from '../../dist/cjs/asynciterable/takewhile';
+import { hasNext, noNext } from '../asynciterablehelpers';
+
+test('AsyncIterable#takeWhile some match', async t => {
+  const xs = of(1, 2, 3, 4);
+  const ys = takeWhile(xs, x => x < 3);
+
+  const it = ys[Symbol.asyncIterator]();
+  await hasNext(t, it, 1);
+  await hasNext(t, it, 2);
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncIterable#takeWhile no match', async t => {
+  const xs = of(1, 2, 3, 4);
+  const ys = takeWhile(xs, () => false);
+
+  const it = ys[Symbol.asyncIterator]();
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncItearble#takeWhile all match', async t => {
+  const xs = of(1, 2, 3, 4);
+  const ys = takeWhile(xs, () => true);
+
+  const it = ys[Symbol.asyncIterator]();
+  await hasNext(t, it, 1);
+  await hasNext(t, it, 2);
+  await hasNext(t, it, 3);
+  await hasNext(t, it, 4);
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncIterable#takeWhile uses index', async t => {
+  const xs = of(1, 2, 3, 4);
+  const ys = takeWhile(xs, (x, i) => i < 2);
+
+  const it = ys[Symbol.asyncIterator]();
+  await hasNext(t, it, 1);
+  await hasNext(t, it, 2);
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncIterable#takeWhile predicate throws', async t => {
+  const err = new Error();
+  const xs = of(1, 2, 3, 4);
+  const ys = takeWhile(xs, () => { throw err; });
+
+  const it = ys[Symbol.asyncIterator]();
+  try {
+    await it.next();
+  } catch (e) {
+    t.same(err, e);
+  }
+  t.end();
+});
