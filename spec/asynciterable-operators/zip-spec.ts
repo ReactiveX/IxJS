52e6eb024695877d4cc6123d42f9290a1c47a5bc
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-08 02:01:16 -0400

    feat(zip) - add async zip

---
+++ spec/asynciterable-operators/zip-spec.ts
@@ -0,0 +1,91 @@
+'use strict';
+
+import * as test from 'tape';
+import { of } from '../../dist/cjs/asynciterable/of';
+import { _throw } from '../../dist/cjs/asynciterable/throw';
+import { zip } from '../../dist/cjs/asynciterable/zip';
+import { hasNext, noNext } from '../asynciterablehelpers';
+
+test('AsyncIterable#zip equal length', async t => {
+  const xs = of(1, 2, 3);
+  const ys = of(4, 5, 6);
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.asyncIterator]();
+  await hasNext(t, it, 1 * 4);
+  await hasNext(t, it, 2 * 5);
+  await hasNext(t, it, 3 * 6);
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncIterable#zip left longer', async t => {
+  const xs = of(1, 2, 3, 4);
+  const ys = of(4, 5, 6);
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.asyncIterator]();
+  await hasNext(t, it, 1 * 4);
+  await hasNext(t, it, 2 * 5);
+  await hasNext(t, it, 3 * 6);
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncIterable#zip right longer', async t => {
+  const xs = of(1, 2, 3);
+  const ys = of(4, 5, 6, 7);
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.asyncIterator]();
+  await hasNext(t, it, 1 * 4);
+  await hasNext(t, it, 2 * 5);
+  await hasNext(t, it, 3 * 6);
+  await noNext(t, it);
+  t.end();
+});
+
+test('AsyncIterable#zip left throws', async t => {
+  const err = new Error();
+  const xs = _throw<number>(err);
+  const ys = of(4, 5, 6);
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.asyncIterator]();
+  try {
+    await it.next();
+  } catch (e) {
+    t.same(err, e);
+  }
+  t.end();
+});
+
+test('AsyncIterable#zip right throws', async t => {
+  const err = new Error();
+  const xs = of(1, 2, 3);
+  const ys = _throw<number>(err);
+  const res = zip(xs, ys, (x, y) => x * y);
+
+  const it = res[Symbol.asyncIterator]();
+  try {
+    await it.next();
+  } catch (e) {
+    t.same(err, e);
+  }
+  t.end();
+});
+
+test('AsyncIterable#zip selector throws', async t => {
+  const err = new Error();
+  const xs = of(1, 2, 3);
+  const ys = of(4, 5, 6);
+  const res = zip(xs, ys, (x, y) => { if (x > 0) { throw err; } return x * y; });
+
+  const it = res[Symbol.asyncIterator]();
+  try {
+    await it.next();
+  } catch (e) {
+    t.same(err, e);
+  }
+  t.end();
+});
