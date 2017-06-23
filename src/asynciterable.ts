b0ab25e715c2868395b5d07b63a00455bed24a46
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-06-23 14:07:52 -0400

    chore(docs) - add basic docs to iterable/async iterable

--- src/asynciterable.ts
+++ src/asynciterable.ts
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * This clas serves as the base for all operations which support [Symbol.asyncIterator].
+ */
 export abstract class AsyncIterableX<T> implements AsyncIterable<T> {
   abstract [Symbol.asyncIterator](): AsyncIterator<T>;
 }
--- src/iterable.ts
+++ src/iterable.ts
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * This clas serves as the base for all operations which support [Symbol.iterator].
+ */
 export abstract class IterableX<T> implements Iterable<T> {
   abstract [Symbol.iterator](): Iterator<T>;
 }
