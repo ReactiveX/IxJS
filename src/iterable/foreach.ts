495e772f1497bc4e8b2be4db5e723a0692313519
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-08-10 16:40:32 -0400

    feat(docs): document for/forEach

--- src/iterable/foreach.ts
+++ src/iterable/foreach.ts
@@ -1,5 +1,11 @@
 import { bindCallback } from '../internal/bindcallback';
 
+/**
+ * Iterates the sequence and invokes the given action for each value in the sequence.
+ * @param {Iterable<T>} source Source sequence.
+ * @param {function:(value: T, index: number): void} callback Action to invoke for each element.
+ * @param {Object} [thisArg] Optional "this" binding for the callback.
+ */
 export function forEach<TSource>(
     source: Iterable<TSource>,
     callback: (value: TSource, index: number) => void,
