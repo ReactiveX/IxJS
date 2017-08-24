d6ee684b543ba3a2479555e996cd90e192721923
Paul Taylor <paul.e.taylor@me.com>
2017-08-24 11:28:22 -0700

    chore(publish): fix prepublish script

--- prepublish.sh
+++ prepublish.sh
@@ -3,12 +3,12 @@ preset=`conventional-commits-detector` && echo $preset
 bump=`conventional-recommended-bump -p $preset` && echo $bump
 npm --no-git-tag-version version $bump &>/dev/null
 conventional-changelog -i CHANGELOG.md -s -p $preset
+npm run validate
+npm run doc
 git add CHANGELOG.md && version=$(json -f package.json version)
 git commit -m "docs(CHANGELOG): $version"
 mv -f _package.json package.json
 npm version $bump -m "chore(release): %s"
 git push --follow-tags
 conventional-github-releaser -p $preset
-npm run validate
-npm run doc
 npm run lerna:publish
