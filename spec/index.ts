5f82fa772ca4ffd76743da8e1e3b83814d3e3f5f
Paul Taylor <paul.e.taylor@me.com>
2017-10-02 02:33:16 -0700

    test(spec): update test files for new build script

--- spec/index.ts
+++ spec/index.ts
@@ -1,4 +1,9 @@
 /* tslint:disable */
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled Rejection at:', p, 'reason:', reason);
+  throw reason;
+});
+
 const cwd = process.cwd();
 const resolve = require('path').resolve;
 require('glob')(`./spec/**/*.ts`, (err: Error, files: string[]) => {
