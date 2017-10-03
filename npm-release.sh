d1d681bd5a7661f5c599e50557e7d929ad1d2ba2
Paul Taylor <paul.e.taylor@me.com>
2017-10-02 18:21:08 -0700

    chore(scripts): fix npm-release script

--- npm-release.sh
+++ npm-release.sh
@@ -4,10 +4,10 @@ bump=${1:-$(conventional-recommended-bump -p $preset)} && echo "semantic-version
 npm --no-git-tag-version version $bump &>/dev/null
 conventional-changelog -i CHANGELOG.md -s -p $preset
 run-s --silent lint build test doc
-git add CHANGELOG.md && version=$(json -f package.json version)
+lerna publish --yes --skip-git --cd-version $bump --force-publish=*
+git add CHANGELOG.md lerna.json && version=$(json -f package.json version)
 git commit -m "docs(CHANGELOG): $version"
 mv -f _package.json package.json
 npm version $bump -m "chore(release): %s"
 git push --follow-tags
 conventional-github-releaser -p $preset
-lerna publish --yes --skip-git --cd-version $bump --force-publish=*
