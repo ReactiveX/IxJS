2a542e5dc1dea682c012b897d357fb77dd40a728
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-05 01:43:27 -0400

    feat(merge): fix indexOf issues with merge

--- src/asynciterable/merge.ts
+++ src/asynciterable/merge.ts
@@ -3,6 +3,12 @@ import { AsyncIterableX } from '../asynciterable';
 // tslint:disable-next-line:no-empty
 const NEVER_PROMISE = new Promise(() => { });
 
+type MergeResult<T> = { value: T, index: number };
+
+function wrapPromiseWithIndex<T>(promise: Promise<T>, index: number) {
+  return promise.then((value) => ({ value, index })) as Promise<MergeResult<T>>;
+}
+
 class MergeAsyncIterable<T> extends AsyncIterableX<T> {
   private _source: AsyncIterable<T>[];
 
@@ -14,24 +20,23 @@ class MergeAsyncIterable<T> extends AsyncIterableX<T> {
   async *[Symbol.asyncIterator](): AsyncIterator<T> {
     const length = this._source.length;
     const iterators = new Array<AsyncIterator<T>>(length);
-    const nexts = new Array<Promise<IteratorResult<T>>>(length);
+    const nexts = new Array<Promise<MergeResult<IteratorResult<T>>>>(length);
     let active = length;
     for (let i = 0; i < length; i++) {
       const iterator = this._source[i][Symbol.asyncIterator]();
       iterators[i] = iterator;
-      nexts[i] = iterator.next();
+      nexts[i] = wrapPromiseWithIndex(iterator.next(), i);
     }
 
     while (active > 0) {
       const next = Promise.race(nexts);
-      const index = nexts.indexOf(next);
-      const next$ = await next;
+      const { value: next$, index } = await next;
       if (next$.done) {
-        nexts[index] = <Promise<IteratorResult<T>>>(NEVER_PROMISE);
+        nexts[index] = <Promise<MergeResult<IteratorResult<T>>>>(NEVER_PROMISE);
         active--;
       } else {
         const iterator$ = iterators[index];
-        nexts[index] = iterator$.next();
+        nexts[index] = wrapPromiseWithIndex(iterator$.next(), index);
         yield next$.value;
       }
     }
