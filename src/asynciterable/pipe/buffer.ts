94cad279d292aa9e281fcfb5b6c0056d2af2c1f5
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-10 00:26:22 -0400

    feat(pipe): add piped operators

---
+++ src/asynciterable/pipe/buffer.ts
@@ -0,0 +1,12 @@
+import { OperatorAsyncFunction } from '../../interfaces';
+import { AsyncIterableX } from '../../asynciterable';
+import { BufferAsyncIterable } from '../buffer';
+
+export function buffer<TSource>(
+    count: number,
+    skip?: number): OperatorAsyncFunction<TSource, TSource[]> {
+  if (skip == null) { skip = count; }
+  return function bufferOperatorFunction(source: AsyncIterable<TSource>): AsyncIterableX<TSource[]> {
+    return new BufferAsyncIterable<TSource>(source, count, skip!);
+  };
+}
---
+++ src/asynciterable/pipe/catchwith.ts
@@ -0,0 +1,10 @@
+import { MonoTypeOperatorAsyncFunction } from '../../interfaces';
+import { AsyncIterableX } from '../../asynciterable';
+import { CatchWithAsyncIterable } from '../catchwith';
+
+export function catchWith<TSource>(
+    handler: (error: any) => AsyncIterable<TSource> | Promise<AsyncIterable<TSource>>): MonoTypeOperatorAsyncFunction<TSource> {
+  return function catchWithOperatorFunction(source: AsyncIterable<TSource>): AsyncIterableX<TSource> {
+    return new CatchWithAsyncIterable<TSource>(source, handler);
+  };
+}
