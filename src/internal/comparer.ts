fdb68851b44bd1535c8a9b288525d2b192bb6788
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-14 13:21:11 -0400

    fix(comparer): Fixes comparer
    Fixes comparer to match `Array.prototype.includes` default comparison

--- src/internal/comparer.ts
+++ src/internal/comparer.ts
@@ -3,12 +3,12 @@
  * @ignore
  */
 export function comparer(a: any, b: any) {
-  return (a === 0 && b === 0) || (a === b || (isNaN(a) && isNaN(b)));
+  return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
 }
 
 /**
  * @ignore
  */
 export async function comparerAsync(a: any, b: any) {
-  return (a === 0 && b === 0) || (a === b || (isNaN(a) && isNaN(b)));
+  return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
 }
