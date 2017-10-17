a2f409bfa1aa67a32c904b5f04be5b7bfc2aafff
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-17 00:00:30 -0400

    feat(merge/concat): adding ignore to overloads

--- src/add/asynciterable-operators/merge.ts
+++ src/add/asynciterable-operators/merge.ts
@@ -2,22 +2,37 @@ import { AsyncIterableX } from '../../asynciterable';
 import { merge } from '../../asynciterable/merge';
 
 /* tslint:disable:max-line-length */
+/**
+ * @ignore
+ */
 export function mergeProto<T>(this: AsyncIterableX<T>): AsyncIterableX<T>;
+/**
+ * @ignore
+ */
 export function mergeProto<T, T2>(
   this: AsyncIterableX<T>,
   v2: AsyncIterable<T2>
 ): AsyncIterableX<T | T2>;
+/**
+ * @ignore
+ */
 export function mergeProto<T, T2, T3>(
   this: AsyncIterableX<T>,
   v2: AsyncIterable<T2>,
   v3: AsyncIterable<T3>
 ): AsyncIterableX<T | T2 | T3>;
+/**
+ * @ignore
+ */
 export function mergeProto<T, T2, T3, T4>(
   this: AsyncIterableX<T>,
   v2: AsyncIterable<T2>,
   v3: AsyncIterable<T3>,
   v4: AsyncIterable<T4>
 ): AsyncIterableX<T | T2 | T3 | T4>;
+/**
+ * @ignore
+ */
 export function mergeProto<T, T2, T3, T4, T5>(
   this: AsyncIterableX<T>,
   v2: AsyncIterable<T2>,
@@ -25,6 +40,9 @@ export function mergeProto<T, T2, T3, T4, T5>(
   v4: AsyncIterable<T4>,
   v5: AsyncIterable<T5>
 ): AsyncIterable<T | T2 | T3 | T4 | T5>;
+/**
+ * @ignore
+ */
 export function mergeProto<T, T2, T3, T4, T5, T6>(
   this: AsyncIterableX<T>,
   v2: AsyncIterable<T2>,
--- src/add/iterable-operators/concat.ts
+++ src/add/iterable-operators/concat.ts
@@ -2,19 +2,31 @@ import { IterableX } from '../../iterable';
 import { concat } from '../../iterable/concat';
 
 /* tslint:disable:max-line-length */
+/**
+ * @ignore
+ */
 export function concatProto<T>(this: IterableX<T>): IterableX<T>;
+/**
+ * @ignore
+ */
 export function concatProto<T, T2>(this: IterableX<T>, v2: Iterable<T2>): IterableX<T | T2>;
 export function concatProto<T, T2, T3>(
   this: IterableX<T>,
   v2: Iterable<T2>,
   v3: Iterable<T3>
 ): IterableX<T | T2 | T3>;
+/**
+ * @ignore
+ */
 export function concatProto<T, T2, T3, T4>(
   this: IterableX<T>,
   v2: Iterable<T2>,
   v3: Iterable<T3>,
   v4: Iterable<T4>
 ): IterableX<T | T2 | T3 | T4>;
+/**
+ * @ignore
+ */
 export function concatProto<T, T2, T3, T4, T5>(
   this: IterableX<T>,
   v2: Iterable<T2>,
@@ -22,6 +34,9 @@ export function concatProto<T, T2, T3, T4, T5>(
   v4: Iterable<T4>,
   v5: Iterable<T5>
 ): Iterable<T | T2 | T3 | T4 | T5>;
+/**
+ * @ignore
+ */
 export function concatProto<T, T2, T3, T4, T5, T6>(
   this: IterableX<T>,
   v2: Iterable<T2>,
