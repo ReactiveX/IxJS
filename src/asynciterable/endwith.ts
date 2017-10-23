25c6f21bb38dcee4910047d1300d2c7dc16d2ddb
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-23 14:10:31 -0400

    feat(pipe): add through endwith

--- src/asynciterable/endwith.ts
+++ src/asynciterable/endwith.ts
@@ -1,6 +1,6 @@
 import { AsyncIterableX } from '../asynciterable';
 
-class EndWithAsyncIterable<TSource> extends AsyncIterableX<TSource> {
+export class EndWithAsyncIterable<TSource> extends AsyncIterableX<TSource> {
   private _source: AsyncIterable<TSource>;
   private _args: TSource[];
 
