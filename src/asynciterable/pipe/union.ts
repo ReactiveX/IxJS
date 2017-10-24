236819d3e530cdfe3dd15e4b2f718f2c32974cfd
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-24 19:54:40 -0400

    feat(pipe): add through union

---
+++ src/asynciterable/pipe/union.ts
@@ -0,0 +1,13 @@
+import { MonoTypeOperatorAsyncFunction } from '../../interfaces';
+import { AsyncIterableX } from '../../asynciterable';
+import { UnionAsyncIterable } from '../union';
+import { comparerAsync } from '../../internal/comparer';
+
+export function union<TSource>(
+  right: AsyncIterable<TSource>,
+  comparer: (x: TSource, y: TSource) => boolean | Promise<boolean> = comparerAsync
+): MonoTypeOperatorAsyncFunction<TSource> {
+  return function unionOperatorFunction(left: AsyncIterable<TSource>): AsyncIterableX<TSource> {
+    return new UnionAsyncIterable<TSource>(left, right, comparer);
+  };
+}
