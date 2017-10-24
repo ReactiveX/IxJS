6ded104bdd5d7e78fd71ebc3387efdfca2f42a7c
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-24 19:21:01 -0400

    feat(pipe): add through takelast

---
+++ src/asynciterable/pipe/takelast.ts
@@ -0,0 +1,11 @@
+import { MonoTypeOperatorAsyncFunction } from '../../interfaces';
+import { AsyncIterableX } from '../../asynciterable';
+import { TakeLastAsyncIterable } from '../takelast';
+
+export function takeLast<TSource>(count: number): MonoTypeOperatorAsyncFunction<TSource> {
+  return function takeLastOperatorFunction(
+    source: AsyncIterable<TSource>
+  ): AsyncIterableX<TSource> {
+    return new TakeLastAsyncIterable<TSource>(source, count);
+  };
+}
