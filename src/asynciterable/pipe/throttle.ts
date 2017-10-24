f44726d677cf0d1040450025d00db48c341fb9cf
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-24 19:42:34 -0400

    feat(pipe): add through throttle

---
+++ src/asynciterable/pipe/throttle.ts
@@ -0,0 +1,11 @@
+import { MonoTypeOperatorAsyncFunction } from '../../interfaces';
+import { AsyncIterableX } from '../../asynciterable';
+import { ThrottleAsyncIterable } from '../throttle';
+
+export function throttle<TSource>(time: number): MonoTypeOperatorAsyncFunction<TSource> {
+  return function throttleOperatorFunction(
+    source: AsyncIterable<TSource>
+  ): AsyncIterableX<TSource> {
+    return new ThrottleAsyncIterable<TSource>(source, time);
+  };
+}
