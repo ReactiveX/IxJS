8f321e1e328372531e1bd7b4e5f19b52dd758e54
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-24 15:56:29 -0400

    feat(pipe): add through scan

---
+++ src/asynciterable/pipe/scan.ts
@@ -0,0 +1,19 @@
+import { OperatorAsyncFunction } from '../../interfaces';
+import { AsyncIterableX } from '../../asynciterable';
+import { ScanAsyncIterable } from '../scan';
+
+export function scan<T>(
+  accumulator: (acc: T, value: T, index: number) => T | Promise<T>
+): OperatorAsyncFunction<T, T>;
+export function scan<T, R = T>(
+  accumulator: (acc: R, value: T, index: number) => R | Promise<R>,
+  seed: R
+): OperatorAsyncFunction<T, R>;
+export function scan<T, R = T>(
+  accumulator: (acc: T | R, value: T, index: number) => R | Promise<R>,
+  ...args: (T | R)[]
+): OperatorAsyncFunction<T, T | R> {
+  return function scanOperatorFunction(source: AsyncIterable<T>): AsyncIterableX<T | R> {
+    return new ScanAsyncIterable(source, accumulator, ...args);
+  };
+}
