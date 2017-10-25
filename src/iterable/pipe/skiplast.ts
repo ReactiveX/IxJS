d148ba34459fc2d2928394cd93ed66f1ff3500cc
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-25 01:19:33 -0400

    feat(pipe): add through skiplast

---
+++ src/iterable/pipe/skiplast.ts
@@ -0,0 +1,9 @@
+import { MonoTypeOperatorFunction } from '../../interfaces';
+import { IterableX } from '../../iterable';
+import { SkipLastIterable } from '../skiplast';
+
+export function skipLast<TSource>(count: number): MonoTypeOperatorFunction<TSource> {
+  return function skipLastOperatorFunction(source: Iterable<TSource>): IterableX<TSource> {
+    return new SkipLastIterable<TSource>(source, count);
+  };
+}
--- src/iterable/skiplast.ts
+++ src/iterable/skiplast.ts
@@ -1,6 +1,6 @@
 import { IterableX } from '../iterable';
 
-class SkipLastIterable<TSource> extends IterableX<TSource> {
+export class SkipLastIterable<TSource> extends IterableX<TSource> {
   private _source: Iterable<TSource>;
   private _count: number;
 
