35d84e96690ecad41415e1f30dc9182c6453e993
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-24 23:18:53 -0400

    feat(pipe): add through endwith

---
+++ src/iterable/pipe/endwith.ts
@@ -0,0 +1,9 @@
+import { MonoTypeOperatorFunction } from '../../interfaces';
+import { IterableX } from '../../iterable';
+import { EndWithIterable } from '../endwith';
+
+export function endWith<TSource>(...args: TSource[]): MonoTypeOperatorFunction<TSource> {
+  return function endsWithOperatorFunction(source: Iterable<TSource>): IterableX<TSource> {
+    return new EndWithIterable<TSource>(source, args);
+  };
+}
