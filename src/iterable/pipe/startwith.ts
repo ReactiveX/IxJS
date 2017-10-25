2e76d23f4962557046dc4ec01694cd18f238eed4
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-25 01:23:30 -0400

    feat(pipe): add through startwith

---
+++ src/iterable/pipe/startwith.ts
@@ -0,0 +1,9 @@
+import { MonoTypeOperatorFunction } from '../../interfaces';
+import { IterableX } from '../../iterable';
+import { StartWithIterable } from '../startwith';
+
+export function startWith<TSource>(...args: TSource[]): MonoTypeOperatorFunction<TSource> {
+  return function startWithOperatorFunction(source: Iterable<TSource>): IterableX<TSource> {
+    return new StartWithIterable<TSource>(source, args);
+  };
+}
