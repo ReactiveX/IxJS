f628703dd9a38166d432fbc16952a601651602fd
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-25 00:40:31 -0400

    feat(pipe): add through minby

---
+++ src/iterable/pipe/minby.ts
@@ -0,0 +1,12 @@
+import { MonoTypeOperatorFunction } from '../../interfaces';
+import { IterableX } from '../../iterable';
+import { extremaBy, defaultCompare } from '../_extremaby';
+
+export function minBy<TSource, TKey>(
+  keySelector: (x: TSource) => TKey,
+  comparer: (x: TKey, y: TKey) => number = defaultCompare
+): MonoTypeOperatorFunction<TSource> {
+  return function minByOperatorFunction(source: Iterable<TSource>): IterableX<TSource> {
+    return extremaBy(source, keySelector, (key, minValue) => -comparer(key, minValue));
+  };
+}
