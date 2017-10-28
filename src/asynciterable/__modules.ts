e05ec2477ce1dd559d0c9b91f6b53618e1cac373
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-27 20:46:17 -0400

    feat(pipe): add to __modules

--- src/asynciterable/__modules.ts
+++ src/asynciterable/__modules.ts
@@ -6,6 +6,8 @@ import { AsyncIterableX } from '../asynciterable';
 import { GroupedAsyncIterable } from './groupby';
 import { OrderedAsyncIterableX } from './orderby';
 import { OrderedAsyncIterableBaseX } from './orderby';
+import { TimeInterval } from './timeinterval';
+import { Timestamp } from './timestamp';
 
 import { asyncify } from './asyncify';
 import { asyncifyErrback } from './asyncifyerrback';
@@ -102,6 +104,9 @@ import { takeUntil } from './takeuntil';
 import { takeWhile } from './takewhile';
 import { tap } from './tap';
 import { throttle } from './throttle';
+import { timeInterval } from './timeinterval';
+import { timeout } from './timeout';
+import { timestamp } from './timestamp';
 import { _throw } from './throw';
 import { toArray } from './toarray';
 import { toMap } from './tomap';
@@ -111,6 +116,64 @@ import { union } from './union';
 import { _while } from './while';
 import { zip } from './zip';
 
+import { buffer as bufferPipe } from './pipe/buffer';
+import { _catch as catchPipe } from './pipe/catch';
+import { catchWith as catchWithPipe } from './pipe/catchwith';
+import { concat as concatPipe } from './pipe/concat';
+import { concatAll as concatAllPipe } from './pipe/concatall';
+import { debounce as debouncePipe } from './pipe/debounce';
+import { defaultIfEmpty as defaultIfEmptyPipe } from './pipe/defaultifempty';
+import { delay as delayPipe } from './pipe/delay';
+import { delayEach as delayEachPipe } from './pipe/delayeach';
+import { distinct as distinctPipe } from './pipe/distinct';
+import { distinctUntilChanged as distinctUntilChangedPipe } from './pipe/distinctuntilchanged';
+import { doWhile as doWhilePipe } from './pipe/dowhile';
+import { endWith as endWithPipe } from './pipe/endwith';
+import { except as exceptPipe } from './pipe/except';
+import { expand as expandPipe } from './pipe/expand';
+import { filter as filterPipe } from './pipe/filter';
+import { _finally as finallyPipe } from './pipe/finally';
+import { flatMap as flatMapPipe } from './pipe/flatmap';
+import { flatten as flattenPipe } from './pipe/flatten';
+import { groupBy as groupByPipe } from './pipe/groupby';
+import { groupJoin as groupJoinPipe } from './pipe/groupjoin';
+import { ignoreElements as ignoreElementsPipe } from './pipe/ignoreelements';
+import { innerJoin as innerJoinPipe } from './pipe/innerjoin';
+import { intersect as intersectPipe } from './pipe/intersect';
+import { map as mapPipe } from './pipe/map';
+import { maxBy as maxByPipe } from './pipe/maxby';
+import { memoize as memoizePipe } from './pipe/memoize';
+import { merge as mergePipe } from './pipe/merge';
+import { mergeAll as mergeAllPipe } from './pipe/mergeall';
+import { minBy as minByPipe } from './pipe/minby';
+import { onErrorResumeNext as onErrorResumeNextPipe } from './pipe/onerrorresumenext';
+import { pairwise as pairwisePipe } from './pipe/pairwise';
+import { pluck as pluckPipe } from './pipe/pluck';
+import { publish as publishPipe } from './pipe/publish';
+import { repeat as repeatPipe } from './pipe/repeat';
+import { retry as retryPipe } from './pipe/retry';
+import { reverse as reversePipe } from './pipe/reverse';
+import { scan as scanPipe } from './pipe/scan';
+import { scanRight as scanRightPipe } from './pipe/scanright';
+import { share as sharePipe } from './pipe/share';
+import { skip as skipPipe } from './pipe/skip';
+import { skipLast as skipLastPipe } from './pipe/skiplast';
+import { skipUntil as skipUntilPipe } from './pipe/skipuntil';
+import { skipWhile as skipWhilePipe } from './pipe/skipwhile';
+import { slice as slicePipe } from './pipe/slice';
+import { startWith as startWithPipe } from './pipe/startwith';
+import { take as takePipe } from './pipe/take';
+import { takeLast as takeLastPipe } from './pipe/takelast';
+import { takeUntil as takeUntilPipe } from './pipe/takeuntil';
+import { takeWhile as takeWhilePipe } from './pipe/takewhile';
+import { tap as tapPipe } from './pipe/tap';
+import { throttle as throttlePipe } from './pipe/throttle';
+import { timeInterval as timeIntervalPipe } from './pipe/timeinterval';
+import { timeout as timeoutPipe } from './pipe/timeout';
+import { timestamp as timestampPipe } from './pipe/timestamp';
+import { union as unionPipe } from './pipe/union';
+import { zip as zipPipe } from './pipe/zip';
+
 export type AsyncIterableX<T> = AsyncIterableX<T>;
 export type Observable<T> = Observable<T>;
 export type NextAsyncObserver<T> = NextAsyncObserver<T>;
@@ -119,6 +182,8 @@ export type CompletionAsyncObserver<T> = CompletionAsyncObserver<T>;
 export type GroupedAsyncIterable<TKey, TValue> = GroupedAsyncIterable<TKey, TValue>;
 export type OrderedAsyncIterableX<TKey, TSource> = OrderedAsyncIterableX<TKey, TSource>;
 export type OrderedAsyncIterableBaseX<TSource> = OrderedAsyncIterableBaseX<TSource>;
+export type TimeInterval<TSource> = TimeInterval<TSource>;
+export type Timestamp<TSource> = Timestamp<TSource>;
 
 export default {
   asyncify,
@@ -216,6 +281,9 @@ export default {
   takeWhile,
   tap,
   throttle,
+  timeInterval,
+  timestamp,
+  timeout,
   _throw,
   toArray,
   toMap,
@@ -223,5 +291,63 @@ export default {
   toSet,
   union,
   _while,
-  zip
+  zip,
+
+  bufferPipe,
+  catchPipe,
+  catchWithPipe,
+  concatPipe,
+  concatAllPipe,
+  debouncePipe,
+  defaultIfEmptyPipe,
+  delayPipe,
+  delayEachPipe,
+  distinctPipe,
+  distinctUntilChangedPipe,
+  doWhilePipe,
+  endWithPipe,
+  exceptPipe,
+  expandPipe,
+  filterPipe,
+  finallyPipe,
+  flatMapPipe,
+  flattenPipe,
+  groupByPipe,
+  groupJoinPipe,
+  ignoreElementsPipe,
+  innerJoinPipe,
+  intersectPipe,
+  mapPipe,
+  maxByPipe,
+  memoizePipe,
+  mergePipe,
+  mergeAllPipe,
+  minByPipe,
+  onErrorResumeNextPipe,
+  pairwisePipe,
+  pluckPipe,
+  publishPipe,
+  repeatPipe,
+  retryPipe,
+  reversePipe,
+  scanPipe,
+  scanRightPipe,
+  sharePipe,
+  skipPipe,
+  skipLastPipe,
+  skipUntilPipe,
+  skipWhilePipe,
+  slicePipe,
+  startWithPipe,
+  takePipe,
+  takeLastPipe,
+  takeUntilPipe,
+  takeWhilePipe,
+  tapPipe,
+  throttlePipe,
+  timeIntervalPipe,
+  timestampPipe,
+  timeoutPipe,
+  unionPipe,
+  zipPipe
 };
--- src/iterable/__modules.ts
+++ src/iterable/__modules.ts
@@ -98,6 +98,55 @@ import { union } from './union';
 import { _while } from './while';
 import { zip } from './zip';
 
+import { buffer as bufferPipe } from './pipe/buffer';
+import { _catch as catchPipe } from './pipe/catch';
+import { catchWith as catchWithPipe } from './pipe/catchwith';
+import { concat as concatPipe } from './pipe/concat';
+import { concatAll as concatAllPipe } from './pipe/concatall';
+import { defaultIfEmpty as defaultIfEmptyPipe } from './pipe/defaultifempty';
+import { distinct as distinctPipe } from './pipe/distinct';
+import { distinctUntilChanged as distinctUntilChangedPipe } from './pipe/distinctuntilchanged';
+import { doWhile as doWhilePipe } from './pipe/dowhile';
+import { endWith as endWithPipe } from './pipe/endwith';
+import { except as exceptPipe } from './pipe/except';
+import { expand as expandPipe } from './pipe/expand';
+import { filter as filterPipe } from './pipe/filter';
+import { _finally as finallyPipe } from './pipe/finally';
+import { flatMap as flatMapPipe } from './pipe/flatmap';
+import { flatten as flattenPipe } from './pipe/flatten';
+import { groupBy as groupByPipe } from './pipe/groupby';
+import { groupJoin as groupJoinPipe } from './pipe/groupjoin';
+import { ignoreElements as ignoreElementsPipe } from './pipe/ignoreelements';
+import { innerJoin as innerJoinPipe } from './pipe/innerjoin';
+import { intersect as intersectPipe } from './pipe/intersect';
+import { map as mapPipe } from './pipe/map';
+import { maxBy as maxByPipe } from './pipe/maxby';
+import { memoize as memoizePipe } from './pipe/memoize';
+import { merge as mergePipe } from './pipe/merge';
+import { mergeAll as mergeAllPipe } from './pipe/mergeall';
+import { minBy as minByPipe } from './pipe/minby';
+import { onErrorResumeNext as onErrorResumeNextPipe } from './pipe/onerrorresumenext';
+import { pairwise as pairwisePipe } from './pipe/pairwise';
+import { pluck as pluckPipe } from './pipe/pluck';
+import { publish as publishPipe } from './pipe/publish';
+import { repeat as repeatPipe } from './pipe/repeat';
+import { retry as retryPipe } from './pipe/retry';
+import { reverse as reversePipe } from './pipe/reverse';
+import { scan as scanPipe } from './pipe/scan';
+import { scanRight as scanRightPipe } from './pipe/scanright';
+import { share as sharePipe } from './pipe/share';
+import { skip as skipPipe } from './pipe/skip';
+import { skipLast as skipLastPipe } from './pipe/skiplast';
+import { skipWhile as skipWhilePipe } from './pipe/skipwhile';
+import { slice as slicePipe } from './pipe/slice';
+import { startWith as startWithPipe } from './pipe/startwith';
+import { take as takePipe } from './pipe/take';
+import { takeLast as takeLastPipe } from './pipe/takelast';
+import { takeWhile as takeWhilePipe } from './pipe/takewhile';
+import { tap as tapPipe } from './pipe/tap';
+import { union as unionPipe } from './pipe/union';
+import { zip as zipPipe } from './pipe/zip';
+
 export type IterableX<T> = IterableX<T>;
 export type Observable<T> = Observable<T>;
 export type NextObserver<T> = NextObserver<T>;
@@ -197,5 +246,54 @@ export default {
   toSet,
   union,
   _while,
-  zip
+  zip,
+
+  bufferPipe,
+  catchPipe,
+  catchWithPipe,
+  concatPipe,
+  concatAllPipe,
+  defaultIfEmptyPipe,
+  distinctPipe,
+  distinctUntilChangedPipe,
+  doWhilePipe,
+  endWithPipe,
+  exceptPipe,
+  expandPipe,
+  filterPipe,
+  finallyPipe,
+  flatMapPipe,
+  flattenPipe,
+  groupByPipe,
+  groupJoinPipe,
+  ignoreElementsPipe,
+  innerJoinPipe,
+  intersectPipe,
+  mapPipe,
+  maxByPipe,
+  memoizePipe,
+  mergePipe,
+  mergeAllPipe,
+  minByPipe,
+  onErrorResumeNextPipe,
+  pairwisePipe,
+  pluckPipe,
+  publishPipe,
+  repeatPipe,
+  retryPipe,
+  reversePipe,
+  scanPipe,
+  scanRightPipe,
+  sharePipe,
+  skipPipe,
+  skipLastPipe,
+  skipWhilePipe,
+  slicePipe,
+  startWithPipe,
+  takePipe,
+  takeLastPipe,
+  takeWhilePipe,
+  tapPipe,
+  unionPipe,
+  zipPipe
 };
