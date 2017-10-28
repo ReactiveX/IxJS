4ecff97997594c4d40fde81840bc3360314cfe0e
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-10-28 18:05:12 -0400

    feat(pipe): adding piped tests

--- spec/asynciterable-operators/buffer-spec.ts
+++ spec/asynciterable-operators/buffer-spec.ts
@@ -1,6 +1,7 @@
 import * as Ix from '../Ix';
 import * as test from 'tape-async';
 const { buffer } = Ix.asynciterable;
+const { bufferPipe } = Ix.asynciterable;
 const { empty } = Ix.asynciterable;
 const { range } = Ix.asynciterable;
 const { sequenceEqual } = Ix.iterable;
@@ -10,6 +11,19 @@ test('AsyncIterable#buffer no skip non-full buffer', async t => {
   const rng = range(0, 10);
 
   const res = await toArray(buffer(rng, 3));
+  t.equal(4, res.length);
+
+  t.true(sequenceEqual(res[0], [0, 1, 2]));
+  t.true(sequenceEqual(res[1], [3, 4, 5]));
+  t.true(sequenceEqual(res[2], [6, 7, 8]));
+  t.true(sequenceEqual(res[3], [9]));
+  t.end();
+});
+
+test('AsyncIterable#bufferPipe no skip non-full buffer', async t => {
+  const rng = range(0, 10);
+
+  const res = await toArray(rng.pipe(bufferPipe(3)));
   t.equal(4, res.length);
 
   t.true(sequenceEqual(res[0], [0, 1, 2]));
@@ -30,10 +44,29 @@ test('AsyncIterable#buffer no skip all full', async t => {
   t.end();
 });
 
+test('AsyncIterable#bufferPipe no skip all full', async t => {
+  const rng = range(0, 10);
+
+  const res = await toArray(rng.pipe(bufferPipe(5)));
+  t.equal(2, res.length);
+
+  t.true(sequenceEqual(res[0], [0, 1, 2, 3, 4]));
+  t.true(sequenceEqual(res[1], [5, 6, 7, 8, 9]));
+  t.end();
+});
+
 test('AsyncIterable#buffer no skip empty buffer', async t => {
   const rng = empty<number>();
 
   const res = await toArray(buffer(rng, 5));
+  t.equal(0, res.length);
+  t.end();
+});
+
+test('AsyncIterable#bufferPipe no skip empty buffer', async t => {
+  const rng = empty<number>();
+
+  const res = await toArray(rng.pipe(bufferPipe(5)));
   t.equal(0, res.length);
   t.end();
 });
@@ -52,10 +85,36 @@ test('AsyncIterable#buffer skip non-full buffer', async t => {
   t.end();
 });
 
+test('AsyncIterable#bufferPipe skip non-full buffer', async t => {
+  const rng = range(0, 10);
+
+  const res = await toArray(rng.pipe(bufferPipe(3, 2)));
+  t.equal(5, res.length);
+
+  t.true(sequenceEqual(res[0], [0, 1, 2]));
+  t.true(sequenceEqual(res[1], [2, 3, 4]));
+  t.true(sequenceEqual(res[2], [4, 5, 6]));
+  t.true(sequenceEqual(res[3], [6, 7, 8]));
+  t.true(sequenceEqual(res[4], [8, 9]));
+  t.end();
+});
+
 test('AsyncIterable#buffer skip full buffer', async t => {
   const rng = range(0, 10);
 
   const res = await toArray(buffer(rng, 3, 4));
+  t.equal(3, res.length);
+
+  t.true(sequenceEqual(res[0], [0, 1, 2]));
+  t.true(sequenceEqual(res[1], [4, 5, 6]));
+  t.true(sequenceEqual(res[2], [8, 9]));
+  t.end();
+});
+
+test('AsyncIterable#bufferPipe skip full buffer', async t => {
+  const rng = range(0, 10);
+
+  const res = await toArray(rng.pipe(bufferPipe(3, 4)));
   t.equal(3, res.length);
 
   t.true(sequenceEqual(res[0], [0, 1, 2]));
