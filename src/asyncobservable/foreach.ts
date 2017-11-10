190bb868da0313ed6c7cc7d5c9a5a625713be9f8
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2017-11-10 00:37:31 -0500

    feat(AsyncObservable): add forEach

---
+++ src/asyncobservable/foreach.ts
@@ -0,0 +1,62 @@
+import { AsyncObserverX } from './asyncobserver';
+import { AsyncSubscription } from './asyncsubscription';
+import { AsyncObservable } from './asyncobservable';
+import { SingleAssignmentAsyncSubscription } from './subscriptions/singleassignmentasyncsubscription';
+import { bindCallback } from '../internal/bindcallback';
+
+class ForEachObserver<T> extends AsyncObserverX<T> {
+  private _onNext: (value: T, index: number) => void;
+  private _resolve: (value?: void | PromiseLike<void> | undefined) => void;
+  private _reject: (reason?: any) => void;
+  private _subscription: AsyncSubscription;
+  private _index: number = 0;
+
+  constructor(
+    onNext: (value: T, index: number) => void,
+    resolve: (value?: void | PromiseLike<void> | undefined) => void,
+    reject: (reason?: any) => void,
+    subscription: AsyncSubscription
+  ) {
+    super();
+    this._onNext = onNext;
+    this._resolve = resolve;
+    this._reject = reject;
+    this._subscription = subscription;
+  }
+
+  async _next(value: T) {
+    try {
+      await this._onNext(value, this._index++);
+    } catch (e) {
+      this._reject(e);
+      await this._subscription.unsubscribe();
+    }
+  }
+
+  async _error(err: any) {
+    this._reject(err);
+    await this._subscription.unsubscribe();
+  }
+
+  async _complete() {
+    this._resolve(undefined);
+    await this._subscription.unsubscribe();
+  }
+}
+
+export function forEach<T>(
+  source: AsyncObservable<T>,
+  onNext: (value: T, index: number) => void,
+  thisArg?: any
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const subscription = new SingleAssignmentAsyncSubscription();
+    const observer = new ForEachObserver<T>(
+      bindCallback(onNext, thisArg, 2),
+      resolve,
+      reject,
+      subscription
+    );
+    source.subscribe(observer).then(s => subscription.assign(s));
+  });
+}
