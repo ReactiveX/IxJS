1c7f98a593f537be615dca354a597ef1e516ae77
ptaylor <paul.e.taylor@me.com>
2019-03-27 18:16:01 -0700

    fix(asynciterable#memoize): ensure memoize and publish source values are only pulled once

--- src/asynciterable/publish.ts
+++ src/asynciterable/publish.ts
@@ -1,67 +1,17 @@
+import { create } from './create';
+import { MemoizeAsyncBuffer } from './memoize';
 import { AsyncIterableX } from './asynciterablex';
 import { RefCountList } from '../iterable/_refcountlist';
-import { create } from './create';
 
-class PublishedAsyncBuffer<T> extends AsyncIterableX<T> {
-  private _buffer: RefCountList<T>;
-  private _source: AsyncIterator<T>;
-  private _error: any;
-  private _stopped: boolean = false;
+class PublishedAsyncBuffer<T> extends MemoizeAsyncBuffer<T> {
+  // @ts-ignore
+  protected _buffer: RefCountList<T>;
 
   constructor(source: AsyncIterator<T>) {
-    super();
-    this._source = source;
-    this._buffer = new RefCountList<T>(0);
+    super(source, new RefCountList<T>(0));
   }
 
-  private async *_getIterable(i: number): AsyncIterable<T> {
-    try {
-      while (1) {
-        let hasValue = false,
-          current = <T>{};
-        if (i >= this._buffer.count) {
-          if (!this._stopped) {
-            try {
-              let next = await this._source.next();
-              hasValue = !next.done;
-              if (hasValue) {
-                current = next.value;
-              }
-            } catch (e) {
-              this._error = e;
-              this._stopped = true;
-            }
-          }
-
-          if (this._stopped) {
-            if (this._error) {
-              throw this._error;
-            } else {
-              break;
-            }
-          }
-
-          if (hasValue) {
-            this._buffer.push(current);
-          }
-        } else {
-          hasValue = true;
-        }
-
-        if (hasValue) {
-          yield this._buffer.get(i);
-        } else {
-          break;
-        }
-
-        i++;
-      }
-    } finally {
-      this._buffer.done();
-    }
-  }
-
-  [Symbol.asyncIterator](): AsyncIterator<T> {
+  [Symbol.asyncIterator]() {
     this._buffer.readerCount++;
     return this._getIterable(this._buffer.count)[Symbol.asyncIterator]();
   }
