0524b8736eebb6b10550ae301aaf14673972f887
Paul Taylor <paul.e.taylor@me.com>
2020-02-24 12:39:05 -0800

    chore(npm-release.sh): require semantic-version bump in npm-release.sh
    Remove angular preset from conventional-changelog-cli to fix changelog, remove semantic-version bump
    detection

--- npm-release.sh
+++ npm-release.sh
@@ -1,5 +1,5 @@
-preset=$(npx conventional-commits-detector) && echo "conventional-commits preset: $preset"
-bump=${1:-$(npx conventional-recommended-bump -p $preset)} && echo "semantic-version bump: $bump"
+bump=${1:?missing required semantic-version bump. usage: \`$0 major|minor|patch\`}
+echo "semantic-version bump: $bump"
 
 npx run-s --silent lint build test
 npx lerna version \
@@ -9,12 +9,12 @@ npx lerna version \
 
 cp package.json _package.json
 npm --no-git-tag-version version $bump &>/dev/null
-npx conventional-changelog -i CHANGELOG.md -s -p $preset
+npx conventional-changelog -i CHANGELOG.md -s
 git add CHANGELOG.md lerna.json && version=$(npx json -f package.json version)
 git commit -m "docs(CHANGELOG): $version" -n
 mv -f _package.json package.json
 npm version $bump -m "chore(release): %s"
 git push --follow-tags
-npx conventional-github-releaser -p $preset
+npx conventional-github-releaser -p
 
 npx lerna exec --no-bail -- npm publish
