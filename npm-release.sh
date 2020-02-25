e499b9277447b8a3a1f932bd7323cc4e6294df0c
Paul Taylor <paul.e.taylor@me.com>
2020-02-25 11:18:09 -0800

    chore(changelog): fix conventional-changelog-angular changelog generation

--- npm-release.sh
+++ npm-release.sh
@@ -9,12 +9,12 @@ npx lerna version \
 
 cp package.json _package.json
 npm --no-git-tag-version version $bump &>/dev/null
-npx conventional-changelog -i CHANGELOG.md -s
+npx conventional-changelog -i CHANGELOG.md -s -p angular
 git add CHANGELOG.md lerna.json && version=$(npx json -f package.json version)
 git commit -m "docs(CHANGELOG): $version" -n
 mv -f _package.json package.json
 npm version $bump -m "chore(release): %s"
 git push --follow-tags
-npx conventional-github-releaser -p
+npx conventional-github-releaser -p angular
 
 npx lerna exec --no-bail -- npm publish
