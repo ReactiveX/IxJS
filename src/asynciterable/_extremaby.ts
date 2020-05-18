4e37f24fc3ba9cf15537a8c5ea5a83d22729c19b
Matthew Podwysocki <matthewp@microsoft.com>
2020-05-18 18:47:04 -0400

    fix(extremaBy): fixing extrema/minBy

--- src/asynciterable/_extremaby.ts
+++ src/asynciterable/_extremaby.ts
@@ -1,11 +1,14 @@
+import { throwIfAborted } from '../aborterror';
 import { wrapWithAbort } from './operators/withabort';
 
 export async function extremaBy<TSource, TKey>(
   source: AsyncIterable<TSource>,
   selector: (item: TSource, signal?: AbortSignal) => TKey | Promise<TKey>,
-  comparer: (left: TKey, right: TKey) => number | Promise<number>,
+  comparer: (left: TKey, right: TKey, signal?: AbortSignal) => number | Promise<number>,
   signal?: AbortSignal
 ): Promise<TSource[]> {
+  throwIfAborted(signal);
+
   let result = [];
   const it = wrapWithAbort(source, signal)[Symbol.asyncIterator]();
   const { value, done } = await it.next();
@@ -18,9 +21,9 @@ export async function extremaBy<TSource, TKey>(
 
   let next: IteratorResult<TSource>;
   while (!(next = await it.next()).done) {
-    const { value: current } = next;
+    const current = next.value;
     const key = await selector(current, signal);
-    const cmp = await comparer(key, resKey);
+    const cmp = await comparer(key, resKey, signal);
 
     if (cmp === 0) {
       result.push(current);
