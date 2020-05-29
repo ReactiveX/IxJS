88dfc2f7350019cdf2b1808c77d2fbabe0037827
ptaylor <paul.e.taylor@me.com>
2020-05-29 01:12:32 -0700

    fix(maxby-spec): fix maxby test

--- spec/iterable/maxby-spec.ts
+++ spec/iterable/maxby-spec.ts
@@ -4,7 +4,7 @@ import { empty, maxBy, sequenceEqual } from 'ix/iterable';
 test('Iterable#maxBy', () => {
   const source = [3, 5, 7, 6, 4, 2];
 
-  const res = maxBy(source, { selector: async (x) => Math.floor(x / 2) });
+  const res = maxBy(source, { selector: (x) => Math.floor(x / 2) });
   expect(sequenceEqual(res, [7, 6])).toBeTruthy();
 });
 
