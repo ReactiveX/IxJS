ea34ad16b80a1f9049f3af7a7d8a59016863bb5a
Matthew Podwysocki <matthewp@microsoft.com>
2020-09-17 13:23:56 -0400

    feat(AsyncObservable): initial commit

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -13,3 +13,31 @@ export type MonoTypeOperatorAsyncFunction<T> = OperatorAsyncFunction<T, T>;
 
 /** @ignore */
 export type BufferLike = string | Buffer | Uint8Array;
+
+export const SYMBOL_ASYNC_DISPOSABLE = Symbol.for('AsyncDisposable');
+
+export interface AsyncSubscription {
+  [SYMBOL_ASYNC_DISPOSABLE]: () => Promise<void>;
+}
+
+export interface AsyncObserver<T> {
+  next: (value: T) => Promise<void>;
+  error: (error: any) => Promise<void>;
+  complete: () => Promise<void>;
+}
+
+export interface AsyncObservable<T> {
+  subscribeAsync: (observer: AsyncObserver<T>) => Promise<AsyncSubscription>;
+}
+
+export interface AsyncSubject<TInput, TOutput = TInput>
+  extends AsyncObserver<TInput>,
+  AsyncObservable<TOutput> {}
+
+export interface AsyncScheduler {
+  now: () => Date;
+  scheduleAsync: (
+    action: (signal: AbortSignal) => Promise<void>,
+    dueTime: number
+  ) => Promise<AsyncSubscription>;
+}
