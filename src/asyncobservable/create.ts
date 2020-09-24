3fc30fb9b547ef8145a02f8504f2b857f2af66fe
Matthew Podwysocki <matthewp@microsoft.com>
2020-09-23 21:49:22 -0400

    feat(schedulers): Fix create with abortsignal

--- src/asyncobservable/create.ts
+++ src/asyncobservable/create.ts
@@ -2,18 +2,25 @@ import { PartialAsyncObserver, AsyncSubscription } from '../interfaces';
 import { AsyncObservableX } from './asyncobservablex';
 
 class AnonymousAsyncObservable<T> extends AsyncObservableX<T> {
-  private _fn: (observer: PartialAsyncObserver<T>) => Promise<AsyncSubscription>;
+  private _fn: (
+    observer: PartialAsyncObserver<T>,
+    signal?: AbortSignal
+  ) => Promise<AsyncSubscription>;
 
-  constructor(fn: (observer: PartialAsyncObserver<T>) => Promise<AsyncSubscription>) {
+  constructor(
+    fn: (observer: PartialAsyncObserver<T>, signal?: AbortSignal) => Promise<AsyncSubscription>
+  ) {
     super();
     this._fn = fn;
   }
 
-  _subscribeAsync(observer: PartialAsyncObserver<T>) {
-    return this._fn(observer);
+  _subscribeAsync(observer: PartialAsyncObserver<T>, signal?: AbortSignal) {
+    return this._fn(observer, signal);
   }
 }
 
-export function create<T>(fn: (observer: PartialAsyncObserver<T>) => Promise<AsyncSubscription>) {
+export function create<T>(
+  fn: (observer: PartialAsyncObserver<T>, signal?: AbortSignal) => Promise<AsyncSubscription>
+) {
   return new AnonymousAsyncObservable<T>(fn);
 }
