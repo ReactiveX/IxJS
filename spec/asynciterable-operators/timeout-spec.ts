f5a213abee816337a35d1ab8566165de02b200ce
Paul Taylor <paul.e.taylor@me.com>
2021-06-09 13:46:36 -0500

    fix(src/asynciterable/operators/timeout.ts): ensure AsyncIterable timeout operator passes its values (#327)
    Typo introduced by linting in a refactor prevented AsyncIterable from passing on its values.
    
    fix #325

---
+++ spec/asynciterable-operators/timeout-spec.ts
@@ -0,0 +1,36 @@
+import { hasNext, noNext, delayValue } from '../asynciterablehelpers';
+import { timeout } from 'ix/asynciterable/operators';
+import { as } from 'ix/asynciterable';
+import { TimeoutError } from 'ix/asynciterable/operators/timeout';
+
+test('AsyncIterable#timeout drops none', async () => {
+  const xs = async function* () {
+    yield await delayValue(1, 50);
+    yield await delayValue(2, 50);
+    yield await delayValue(3, 50);
+  };
+  const ys = as(xs()).pipe(timeout(100));
+
+  const it = ys[Symbol.asyncIterator]();
+  await hasNext(it, 1);
+  await hasNext(it, 2);
+  await hasNext(it, 3);
+  await noNext(it);
+});
+
+test('AsyncIterable#timeout throws when delayed', async () => {
+  const xs = async function* () {
+    yield await delayValue(1, 50);
+    yield await delayValue(2, 200);
+  };
+  const ys = as(xs()).pipe(timeout(100));
+
+  const it = ys[Symbol.asyncIterator]();
+  await hasNext(it, 1);
+  try {
+    await it.next();
+  } catch (e) {
+    expect(e).toBeInstanceOf(TimeoutError);
+  }
+  await noNext(it);
+});
