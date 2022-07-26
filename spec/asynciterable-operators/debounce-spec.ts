aafd0fb8ebeb9131278cee69ecfbc2ee73d44111
ptaylor <paul.e.taylor@me.com>
2022-07-26 14:43:31 -0700

    test(debounce): test that debounce triggers finalize

--- spec/asynciterable-operators/debounce-spec.ts
+++ spec/asynciterable-operators/debounce-spec.ts
@@ -1,5 +1,5 @@
-import { hasNext, noNext, delayValue } from '../asynciterablehelpers';
-import { debounce } from 'ix/asynciterable/operators';
+import { hasNext, hasErr, noNext, delayError, delayValue } from '../asynciterablehelpers';
+import { debounce, finalize } from 'ix/asynciterable/operators';
 import { as } from 'ix/asynciterable';
 import { AbortError } from 'ix/Ix';
 
@@ -53,8 +53,59 @@ test(
     const it = ys[Symbol.asyncIterator](controller.signal);
     await hasNext(it, 1);
     setImmediate(() => controller.abort());
-    await expect(hasNext(it, 3)).rejects.toThrow(AbortError);
+    await hasErr(it, AbortError);
     await noNext(it);
+  },
+  10 * 1000
+);
+
+test(
+  'AsyncIterable#debounce triggers finalize on error',
+  async () => {
+    let done = false;
+    const e = new Error();
+    const xs = async function* () {
+      yield await delayValue(1, 100);
+      yield await delayError(e, 100);
+      yield await delayValue(3, 100);
+    };
+    const ys = as(xs()).pipe(
+      finalize(() => {
+        done = true;
+      }),
+      debounce(50)
+    );
+
+    const it = ys[Symbol.asyncIterator]();
+    await hasNext(it, 1);
+    await hasErr(it, e);
+    await noNext(it);
+    expect(done).toBeTruthy();
+  },
+  10 * 1000
+);
+
+test(
+  'AsyncIterable#debounce triggers finalize on complete',
+  async () => {
+    let done = false;
+    const xs = async function* () {
+      yield await delayValue(1, 200);
+      yield await delayValue(2, 400);
+      yield await delayValue(3, 200);
+    };
+    const ys = as(xs()).pipe(
+      finalize(() => {
+        done = true;
+      }),
+      debounce(300)
+    );
+
+    const it = ys[Symbol.asyncIterator]();
+    await hasNext(it, 1);
+    await hasNext(it, 3);
+    await noNext(it);
+    expect(done).toBeTruthy();
   },
   10 * 1000
 );
