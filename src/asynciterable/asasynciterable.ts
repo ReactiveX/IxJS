cdd7942ae24473ca44b15f04f75ecdb0f15c56f4
ptaylor <paul.e.taylor@me.com>
2023-12-28 22:38:31 -0800

    convert AsyncIterableTransform to a constructor function

--- src/asynciterable/asasynciterable.ts
+++ src/asynciterable/asasynciterable.ts
@@ -2,7 +2,11 @@ import { AsyncIterableX } from './asynciterablex';
 import { OperatorAsyncFunction, UnaryFunction } from '../interfaces';
 import { Transform, TransformCallback, TransformOptions } from 'stream';
 
-export interface AsyncIterableTransform<T> extends AsyncIterableX<T>, NodeJS.ReadableStream, NodeJS.WritableStream {
+export interface AsyncIterableTransform<T>
+  extends AsyncIterableX<T>,
+    NodeJS.ReadableStream,
+    NodeJS.WritableStream {
+  new (options?: TransformOptions): AsyncIterableTransform<T>;
   pipe<R>(...operations: UnaryFunction<AsyncIterable<T>, R>[]): R;
   pipe<R>(...operations: OperatorAsyncFunction<T, R>[]): AsyncIterableX<R>;
   pipe<R extends NodeJS.WritableStream>(writable: R, options?: { end?: boolean }): R;
@@ -11,35 +15,39 @@ export interface AsyncIterableTransform<T> extends AsyncIterableX<T>, NodeJS.Rea
 
 const asyncIterableMixin = Symbol('asyncIterableMixin');
 
-export class AsyncIterableTransform<T> {
-  private static [asyncIterableMixin] = false;
-  constructor(options?: TransformOptions) {
-    Transform.call(this as any, options);
-    // If this is the first time AsyncIterableTransform is being constructed,
-    // mixin the methods from the AsyncIterableX's prototype.
-    if (!AsyncIterableTransform[asyncIterableMixin]) {
-      AsyncIterableTransform[asyncIterableMixin] = true;
-      Object.defineProperties(
-        AsyncIterableTransform.prototype,
-        Object.getOwnPropertyDescriptors(AsyncIterableX.prototype)
-      );
-    }
-  }
-  /** @nocollapse */
-  _flush(callback: TransformCallback): void {
-    callback();
-  }
-  /** @nocollapse */
-  _transform(chunk: any, _encoding: string, callback: TransformCallback): void {
-    callback(null, chunk);
+export function AsyncIterableTransform<T>(
+  this: AsyncIterableTransform<T>,
+  options?: TransformOptions
+) {
+  Transform.call(this as any, options);
+  // If this is the first time AsyncIterableTransform is being constructed,
+  // mixin the methods from the AsyncIterableX's prototype.
+  if (!AsyncIterableTransform[asyncIterableMixin]) {
+    AsyncIterableTransform[asyncIterableMixin] = true;
+    Object.defineProperties(
+      AsyncIterableTransform.prototype,
+      Object.getOwnPropertyDescriptors(AsyncIterableX.prototype)
+    );
   }
 }
 
-(AsyncIterableTransform as any).prototype = Object.create(
-  Transform.prototype,
-  Object.getOwnPropertyDescriptors(AsyncIterableTransform.prototype)
-);
+AsyncIterableTransform.prototype = Object.create(Transform.prototype);
+
+AsyncIterableTransform[asyncIterableMixin] = false;
+
+/** @nocollapse */
+AsyncIterableTransform.prototype._flush = function (callback: TransformCallback): void {
+  callback();
+};
+/** @nocollapse */
+AsyncIterableTransform.prototype._transform = function (
+  chunk: any,
+  _encoding: string,
+  callback: TransformCallback
+): void {
+  callback(null, chunk);
+};
 
 export function asAsyncIterable<T>(options: TransformOptions = {}) {
-  return new AsyncIterableTransform<T>(options);
+  return Reflect.construct(AsyncIterableTransform, [options]) as AsyncIterableTransform<T>;
 }
