f25687b46a547b510184ff6558884628e37063b5
ptaylor <paul.e.taylor@me.com>
2024-07-10 13:49:58 -0700

    fix(changelog): include latest changelog in npm packages

--- npm-release.sh
+++ npm-release.sh
@@ -1,7 +1,13 @@
+#! /usr/bin/env bash
+
 set -x;
 
 bump=${1:?missing required semantic-version bump. usage: \`$0 major|minor|patch\`}
 echo "semantic-version bump: $bump"
+
+cp {,_}package.json
+npm --no-git-tag-version version "$bump" &>/dev/null
+npx conventional-changelog -i CHANGELOG.md -s -p angular
 
 npm run --silent lint
 npm run --silent build
@@ -9,19 +15,15 @@ npm run --silent test
 npx lerna version \
     --yes --force-publish=* \
     --no-push --no-git-tag-version \
-    $bump
+    "$bump"
 
-cp package.json _package.json
-npm --no-git-tag-version version $bump &>/dev/null
-npx conventional-changelog -i CHANGELOG.md -s -p angular
 git add CHANGELOG.md lerna.json
-version=$(npx --yes json -f package.json version)
-git commit -m "docs(CHANGELOG): $version" -n
-mv -f _package.json package.json
-npm version $bump -m "chore(release): %s"
+git commit -m "docs(CHANGELOG): $(npx --yes json -f package.json version)" -n
+mv -f {_,}package.json
+npm version "$bump" -m "chore(release): %s"
 git push --follow-tags
 npx conventional-github-releaser -p angular
 
 read -p "Please enter your npm 2FA one-time password: " NPM_OTP </dev/tty
 
-npx lerna exec --concurrency 1 --no-bail -- npm publish --otp=$NPM_OTP
+npx lerna exec --concurrency 1 --no-bail -- npm publish --otp="$NPM_OTP"
