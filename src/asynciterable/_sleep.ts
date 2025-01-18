d9004063fe79d38da9417adc3072978976847f0b
Jesper Engberg <jeengbe@gmail.com>
2025-01-18 20:16:21 +0100

    feat(interval): option to unref sleep interval

--- src/asynciterable/_sleep.ts
+++ src/asynciterable/_sleep.ts
@@ -1,7 +1,7 @@
 import { AbortError } from '../aborterror.js';
 
-export function sleep(dueTime: number, signal?: AbortSignal) {
-  return new Promise<void>((resolve, reject) => {
+export function sleep(dueTime: number, signal?: AbortSignal, unref = false): Promise<void> {
+  return new Promise((resolve, reject) => {
     if (signal && signal.aborted) {
       reject(new AbortError());
     }
@@ -17,6 +17,10 @@ export function sleep(dueTime: number, signal?: AbortSignal) {
 
       resolve();
     }, dueTime);
+
+    if (unref && typeof id['unref'] === 'function') {
+      id['unref']();
+    }
 
     if (signal) {
       signal.addEventListener('abort', onAbort, { once: true });
