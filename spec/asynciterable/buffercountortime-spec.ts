ca7d88c47a31cab1d00dff0badf6219d6d2ff75c
Jesper Engberg <jeengbe@gmail.com>
2025-02-09 19:49:00 +0100

    chore: add more tests

--- spec/asynciterable/buffercountortime-spec.ts
+++ spec/asynciterable/buffercountortime-spec.ts
@@ -1,6 +1,6 @@
 import '../asynciterablehelpers.js';
-import { of, concat, toArray } from 'ix/asynciterable/index.js';
-import { bufferCountOrTime, delay } from 'ix/asynciterable/operators/index.js';
+import { of, concat, toArray, interval } from 'ix/asynciterable/index.js';
+import { bufferCountOrTime, delay, take } from 'ix/asynciterable/operators/index.js';
 
 test('buffer count behaviour', async () => {
   const source = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
@@ -19,4 +19,12 @@ test('buffer time behaviour', async () => {
   const res = source.pipe(bufferCountOrTime(5, 10));
 
   expect(await toArray(res)).toEqual([[1, 2, 3, 4, 5], [6, 7, 8, 9], [0]]);
+});
+
+test('continues the timer in the background', async () => {
+  const source = interval(10);
+
+  const res = source.pipe(bufferCountOrTime(3, 45));
+
+  expect(await toArray(res.pipe(take(2)))).toEqual([[0, 1, 2], [3]]);
 });
