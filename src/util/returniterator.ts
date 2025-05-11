5b7c06a3bd1fbd11b2c2d634e2b11d2e4bd8bd33
Jesper Engberg <jeengbe@gmail.com>
2025-05-11 19:35:50 +0200

    chore: comment wording

--- src/util/returniterator.ts
+++ src/util/returniterator.ts
@@ -15,7 +15,7 @@ export async function returnAsyncIterators(iterators: AsyncIterator<unknown>[]):
     // Calling return on a generator that is currently executing should throw a TypeError, so we can't
     // just await the return call of any iterator. To be fully correct, we should instead abort instead
     // of returning in most situations, but for now, this will do.
-    // TODO: Send a signal to the other iterators to stop
+    // TODO: Send a signal to the pending `next()` Promises to stop
     void iterator.return?.();
   }
 }
